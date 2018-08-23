31e2c90f2dfd59ae1ce675d68c6ada04a8de00df
Ludwig <Blacklightinglr@gmail.com>
2018-08-22 21:32:32 -0400

    update demo.gif

--- lib/cards/events.dart
+++ lib/cards/events.dart
@@ -42,61 +42,53 @@ Widget _factionEvents(BuildContext context, NavisModel model) {
 
   return Tiles(
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child:
+    padding: const EdgeInsets.all(8.0),
+    child:
         Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          Container(
-              child: Text(model.events[0].description,
-                  style: TextStyle(fontSize: 20.0))),
-          Divider(color: Theme
-              .of(context)
-              .accentColor),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                    child: Stack(
-                        alignment: Alignment.center, children: <Widget>[
-                      percent,
-                      checkFaction
-                          ? Icon(ImageAssets.corpus, size: 60.0)
-                          : Icon(ImageAssets.grineer, size: 60.0)
-                    ])),
-                Column(crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(children: <Widget>[
-                        Container(
-                            padding: EdgeInsets.all(5.0),
-                            decoration: BoxDecoration(
-                                color: Colors.red,
-                                borderRadius: BorderRadius.circular(8.0)),
-                            child: Text(model.events[0].victimNode)),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Container(
-                            padding: EdgeInsets.all(5.0),
-                            decoration: BoxDecoration(
-                                color: Colors.orange,
-                                borderRadius: BorderRadius.circular(8.0)),
-                            child: Text(
-                                '${model.events[0].health}\% Remaining'),
-                          ),
-                        ),
-                      ]),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8.0),
-                        child: Container(
-                          padding: EdgeInsets.all(5.0),
-                          decoration: BoxDecoration(
-                              color: Colors.green,
-                              borderRadius: BorderRadius.circular(8.0)),
-                          child: Text(
-                              '${model.events[0].rewards[0]
-                                  .itemString} + ${model.events[0].rewards[0]
-                                  .credits}cr'),
-                        ),
-                      ),
-                    ]),
-              ])
+      Container(
+          child: Text(model.events[0].description,
+              style: TextStyle(fontSize: 20.0))),
+      Divider(color: Theme.of(context).accentColor),
+      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[
+        Container(
+            child: Stack(alignment: Alignment.center, children: <Widget>[
+          percent,
+          checkFaction
+              ? Icon(ImageAssets.corpus, size: 60.0)
+              : Icon(ImageAssets.grineer, size: 60.0)
+        ])),
+        Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+          Row(children: <Widget>[
+            Container(
+                padding: EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8.0)),
+                child: Text(model.events[0].victimNode)),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Container(
+                padding: EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(8.0)),
+                child: Text('${model.events[0].health}\% Remaining'),
+              ),
+            ),
+          ]),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Container(
+              padding: EdgeInsets.all(5.0),
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(8.0)),
+              child: Text(
+                  '${model.events[0].rewards[0].itemString} + ${model.events[0].rewards[0].credits}cr'),
+            ),
+          ),
         ]),
-      ));
+      ])
+    ]),
+  ));
 }
