af23fa08555c989871bc889ffe6ee54ae0635ecc
Ludwig <blacklightinglr@gmail.com>
2018-11-20 13:48:24 -0500

    fixed sentry not reporting in release mode

--- lib/network/sentry.dart
+++ lib/network/sentry.dart
@@ -6,78 +6,75 @@ import 'package:sentry/sentry.dart';
 import '../resources/keys.dart';
 
 class ExceptionService {
-  final SentryClient _sentry =
-      SentryClient(dsn: dsn, environmentAttributes: Event(release: '1.0.6'));
+  final SentryClient sentry;
 
-  static bool isDebug = true;
+  static String appVersion;
+  static String androidVersion;
 
-  // Singleton
+  static bool isDebug = false;
 
-  static final ExceptionService _exceptionService =
-      ExceptionService._internal();
+  factory ExceptionService() {
+    final sentry = SentryClient(
+        dsn: dsn,
+        environmentAttributes: Event(
+            release: appVersion, extra: {'Android Version': androidVersion}));
 
-  factory ExceptionService() => _exceptionService;
-
-  ExceptionService._internal() {
     assert(isDebug = true);
+    FlutterError.onError = (FlutterErrorDetails details) async {
+      if (isDebug) {
+        FlutterError.dumpErrorToConsole(details);
+      } else {
+        Zone.current.handleUncaughtError(details.exception, details.stack);
+      }
+    };
 
-    if (!isDebug) {
-      FlutterError.onError = (FlutterErrorDetails details) async {
-        debugPrint('FlutterError.onError caught an error');
-
-        await reportErrorAndStackTrace(details.exception, details.stack);
-      };
-    }
+    return ExceptionService._(sentry);
   }
+
+  ExceptionService._(this.sentry);
 
   /// Reports [error] (either an [Exception] or an [Error]) to Sentry.io.
 
   Future<Null> reportError(dynamic error) async {
-    if (!isDebug) {
-      debugPrint('Caught error: $error');
+    if (isDebug) throw error;
 
-      debugPrint('Reporting to Sentry.io...');
+    debugPrint('Caught error: $error');
+    debugPrint('Reporting to Sentry.io...');
 
-      SentryResponse response;
+    SentryResponse response;
 
-      if (error is Error) {
-        response = await _sentry.captureException(
-          exception: error,
-          stackTrace: error.stackTrace,
-        );
-      } else {
-        response = await _sentry.captureException(exception: error);
-      }
-
-      if (response.isSuccessful) {
-        debugPrint('Success! Event ID: ${response.eventId}');
-      } else {
-        debugPrint('Failed to report to Sentry.io: ${response.error}');
-      }
+    if (error is Error) {
+      response = await sentry.captureException(
+        exception: error,
+        stackTrace: error.stackTrace,
+      );
     } else {
-      throw error;
+      response = await sentry.captureException(exception: error);
+    }
+
+    if (response.isSuccessful) {
+      debugPrint('Success! Event ID: ${response.eventId}');
+    } else {
+      debugPrint('Failed to report to Sentry.io: ${response.error}');
     }
   }
 
   Future<Null> reportErrorAndStackTrace(
       dynamic error, dynamic stackTrace) async {
-    if (!isDebug) {
-      debugPrint('Caught error: $error');
+    if (isDebug) throw error;
 
-      debugPrint('Reporting to Sentry.io...');
+    debugPrint('Caught error: $error');
+    debugPrint('Reporting to Sentry.io...');
 
-      final SentryResponse response = await _sentry.captureException(
-        exception: error,
-        stackTrace: stackTrace,
-      );
+    final SentryResponse response = await sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace,
+    );
 
-      if (response.isSuccessful) {
-        debugPrint('Success! Event ID: ${response.eventId}');
-      } else {
-        debugPrint('Failed to report to Sentry.io: ${response.error}');
-      }
+    if (response.isSuccessful) {
+      debugPrint('Success! Event ID: ${response.eventId}');
     } else {
-      throw error;
+      debugPrint('Failed to report to Sentry.io: ${response.error}');
     }
   }
 }
