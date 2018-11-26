46334cf6f3c4ee41af033fe935bc157d728ea37f
Ludwig <blacklightinglr@gmail.com>
2018-11-25 22:01:39 -0500

    renamed rewardpool

--- lib/models/export.dart
+++ lib/models/export.dart
@@ -7,7 +7,7 @@ export 'package:navis/models/worldstate/orbvallis.dart';
 export 'worldstate/invasions.dart';
 export 'worldstate/news.dart';
 export 'worldstate/persistentEnemies.dart';
-export 'worldstate/rewardpool.dart';
+export 'worldstate/rewards.dart';
 export 'worldstate/sortie.dart';
 export 'worldstate/syndicates.dart';
 export 'worldstate/trader.dart';
