6dbc2e5d3f2cdd91c79a58842f41c0d8427189c5
Ludwig <blacklightinglr@gmail.com>
2019-01-24 00:27:43 -0500

    reduce, reuse, recycle

---
+++ lib/ui/widgets/row_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class RowItem extends StatelessWidget {
+  final List<Widget> icons;
+  final String text;
+  final bool caption;
+  final Widget child;
+
+  RowItem(
+      {this.icons = const <Widget>[],
+      @required this.text,
+      this.caption = false,
+      @required this.child});
+
+  factory RowItem.richText(
+      {String title, String richText, Color color, double size}) {
+    return RowItem(
+        text: title,
+        child: Text(richText,
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: size, color: color)));
+  }
+
+  _addIcons(bool notEmpty, List<Widget> children) {
+    if (notEmpty)
+      children.insertAll(
+          0,
+          icons.map(
+              (i) => Padding(padding: EdgeInsets.only(right: 4.0), child: i)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final style = caption
+        ? Theme.of(context).textTheme.caption.copyWith(fontSize: 13)
+        : Theme.of(context).textTheme.subhead;
+
+    List<Widget> children = [Text(text, style: style)];
+    _addIcons(icons.isNotEmpty, children);
+
+    final _text = Container(child: Row(children: children));
+
+    return Container(
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[Expanded(child: _text), child]));
+  }
+}
--- lib/ui/widgets/static_box.dart
+++ lib/ui/widgets/static_box.dart
@@ -5,15 +5,26 @@ class StaticBox extends StatelessWidget {
   final Widget child;
   final EdgeInsetsGeometry padding;
 
-  StaticBox(
-      {@required this.child,
-      this.color,
-      this.padding = const EdgeInsets.all(4.0)});
+  StaticBox({this.child, this.color, this.padding = const EdgeInsets.all(3.0)});
+
+//create simple text box with hard coded color of white
+  factory StaticBox.text(
+      {String text,
+      Color color,
+      double size,
+      EdgeInsets padding = const EdgeInsets.all(3.0)}) {
+    return StaticBox(
+      padding: padding,
+      color: color,
+      child: Text(text, style: TextStyle(color: Colors.white, fontSize: size)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: padding,
+        padding: EdgeInsets.all(4.0),
+        margin: padding,
         decoration: BoxDecoration(
             color: color, borderRadius: BorderRadius.all(Radius.circular(3.0))),
         child: child);
