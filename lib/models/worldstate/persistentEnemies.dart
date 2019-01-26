8a82a5f6201d3208967300db9f9d2e57de3f3833
Ludwig <blacklightinglr@gmail.com>
2019-01-26 06:26:34 -0500

    tidy up of acolytes

--- lib/models/worldstate/persistentEnemies.dart
+++ lib/models/worldstate/persistentEnemies.dart
@@ -1,8 +1,9 @@
 import 'package:codable/codable.dart';
 
 class PersistentEnemies extends Coding {
-  String agentType, locationTag, lastDiscoveredTime, lastDiscoveredAt;
-  int fleeDamage, region;
+  String agentType, locationTag, lastDiscoveredAt;
+  DateTime lastDiscoveredTime;
+  int fleeDamage, region, rank;
   num healthPercent;
   bool isDiscovered, isUsingTicketing;
 
@@ -12,11 +13,12 @@ class PersistentEnemies extends Coding {
 
     agentType = object.decode('agentType');
     locationTag = object.decode('locationTag');
-    lastDiscoveredTime = object.decode('lastDiscoveredTime');
+    lastDiscoveredTime = DateTime.parse(object.decode('lastDiscoveredTime'));
     lastDiscoveredAt = object.decode('lastDiscoveredAt');
 
     fleeDamage = object.decode('fleeDamage');
     region = object.decode('region');
+    rank = object.decode('rank');
     healthPercent = object.decode('healthPercent');
     isDiscovered = object.decode('isDiscovered');
     isUsingTicketing = object.decode('isUsingTicketing');
@@ -31,6 +33,7 @@ class PersistentEnemies extends Coding {
 
     object.encode('fleeDamage', fleeDamage);
     object.encode('region', region);
+    object.encode('rank', rank);
     object.encode('healthPercent', healthPercent);
     object.encode('isDiscovered', isDiscovered);
     object.encode('isUsingTicketing', isUsingTicketing);
--- lib/ui/screens/feed/cards/acolytes.dart
+++ lib/ui/screens/feed/cards/acolytes.dart
@@ -4,100 +4,70 @@ import 'package:navis/blocs/worldstate_bloc.dart';
 import 'package:navis/models/export.dart';
 
 import 'package:navis/ui/widgets/cards.dart';
+import 'package:navis/ui/widgets/row_item.dart';
+import 'package:navis/ui/widgets/static_box.dart';
 
 class Acolytes extends StatelessWidget {
-  Widget _buildAcolytes(PersistentEnemies enemy) {
-    return Container(
-        padding: EdgeInsets.all(8.0),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: Container(
-              child: Stack(
-                  alignment: Alignment.center,
-                  children: <Widget>[acolyteProfile(enemy.agentType)]),
-            ),
-          ),
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(children: <Widget>[
-                  Padding(
-                      padding: EdgeInsets.only(bottom: 5.0, right: 5.0),
-                      child: Container(
-                        padding: EdgeInsets.all(5.0),
-                        decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(5.0)),
-                        child: Text('Acolyte: ${enemy.agentType}'),
-                      )),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 5.0),
-                    child: Container(
-                      padding: EdgeInsets.all(5.0),
-                      decoration: BoxDecoration(
-                          color: Colors.red[800],
-                          borderRadius: BorderRadius.circular(5.0)),
-                      child: Text(
-                          'Health: ${(enemy.healthPercent * 100).toStringAsFixed(2)}\%'),
-                    ),
-                  )
-                ]),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 5.0),
-                  child: Row(children: <Widget>[
-                    Container(
-                        padding: EdgeInsets.all(5.0),
-                        decoration: BoxDecoration(
-                            color:
-                                enemy.isDiscovered ? Colors.green : Colors.grey,
-                            borderRadius: BorderRadius.circular(5.0)),
-                        child: enemy.isDiscovered
-                            ? Text('Located on: ${enemy.lastDiscoveredAt}')
-                            : Text('Last seen on: ${enemy.lastDiscoveredAt}'))
-                  ]),
-                ),
-              ]),
-        ]));
-  }
-
   @override
   Widget build(BuildContext context) {
     final enemies = BlocProvider.of<WorldstateBloc>(context);
 
     return StreamBuilder<WorldState>(
+      initialData: WorldstateBloc.initworldstate,
       stream: enemies.worldstate,
       builder: (BuildContext context, AsyncSnapshot<WorldState> snapshot) {
         return Tiles(
+            title: 'Acolytes',
             child: Column(
                 children: snapshot.data.persistentEnemies
-                    .map(_buildAcolytes)
+                    .map((e) => AcolyteProfile(enemy: e))
                     .toList()));
       },
     );
   }
 }
 
-acolyteProfile(String acolyte) {
-  switch (acolyte) {
-    case 'Angst':
-      return Image.asset('assets/acolytes/StrikerAcolyte.png', scale: 5.0);
-      break;
-    case 'Malice':
-      return Image.asset('assets/acolytes/HeavyAcolyte.png', scale: 5.0);
-      break;
-    case 'Mania':
-      return Image.asset('assets/acolytes/RogueAcolyte.png', scale: 5.0);
-      break;
-    case 'Misery':
-      return Image.asset('assets/acolytes/AreaCasterAcolyte.png', scale: 5.0);
-      break;
-    case 'Torment':
-      return Image.asset('assets/acolytes/ControlAcolyte.png', scale: 5.0);
-      break;
-    case 'Violence':
-      return Image.asset('assets/acolytes/DuellistAcolyte.png', scale: 4.0);
-      break;
+class AcolyteProfile extends StatelessWidget {
+  final PersistentEnemies enemy;
+
+  AcolyteProfile({@required this.enemy});
+
+  _healthColor(double health) {
+    if (health > 50.0)
+      return Colors.green;
+    else if (health <= 50.0 && health >= 10.0)
+      return Colors.orange[700];
+    else if (health < 10.0) return Colors.red;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      RowItem(
+          text: 'Acolyte: ${enemy.agentType} | lvl: ${enemy.rank}',
+          child: StaticBox.text(
+              size: 15,
+              text:
+                  'Health: ${(enemy.healthPercent * 100).toStringAsFixed(2)}\%',
+              color: _healthColor((enemy.healthPercent * 100)))),
+      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[
+        StaticBox(
+          color: enemy.isDiscovered ? Colors.red[800] : Colors.grey,
+          child: Row(children: <Widget>[
+            enemy.isDiscovered
+                ? Icon(Icons.gps_fixed)
+                : Icon(Icons.gps_not_fixed),
+            SizedBox(width: 4),
+            Text(enemy.lastDiscoveredAt)
+          ]),
+        ),
+        StaticBox.text(
+          color: enemy.isDiscovered ? Colors.red[800] : Colors.blueAccent[400],
+          size: 15,
+          text:
+              '${enemy.isDiscovered ? 'Found' : 'Last seen'}: ${enemy.lastDiscoveredTime.difference(DateTime.now()).inMinutes.abs()}m ago',
+        )
+      ])
+    ]);
   }
 }
