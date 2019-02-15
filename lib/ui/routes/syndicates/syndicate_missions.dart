291177ce57823836ef6d6c6328195f94d1ce7f8e
Ludwig <blacklightinglr@gmail.com>
2019-02-15 15:19:14 -0500

    remove bounty reward images and put rewards and missions on one page

--- lib/ui/routes/syndicates/syndicate_missions.dart
+++ lib/ui/routes/syndicates/syndicate_missions.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:navis/ui/widgets/icons.dart';
 import 'package:navis/blocs/bloc.dart';
 import 'package:navis/models/export.dart';
 import 'package:navis/utils/factionutils.dart';
-import 'package:navis/ui/widgets/cards.dart';
-import 'rewards.dart';
+import 'package:sticky_headers/sticky_headers.dart';
 
 class SyndicateJobs extends StatefulWidget {
   const SyndicateJobs({this.faction, this.events});
@@ -33,39 +33,46 @@ class SyndicateJobsState extends State<SyndicateJobs> {
                 return const Center(child: CircularProgressIndicator());
 
               if (state is WorldstateLoaded) {
-                final List<Syndicates> syndicate = state.worldState.syndicates
-                    .where((syn) =>
-                        syn.syndicate == futils.factionCheck(widget.faction))
-                    .toList();
+                final Syndicates syndicate = state.worldState.syndicates
+                    .firstWhere((syn) =>
+                        syn.syndicate == futils.factionCheck(widget.faction));
 
-                return ListView(
-                    children: syndicate[0]
-                        .jobs
-                        .map((j) => _buildMissionType(context, j))
-                        .toList());
+                return ListView.builder(
+                    itemCount: syndicate.jobs.length,
+                    itemBuilder: (_, int index) {
+                      final job = syndicate.jobs[index];
+
+                      return StickyHeader(
+                        header:
+                            _buildMissionType(job, futils, widget.faction, _),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: job.rewardPool
+                              .map((r) => ListTile(title: Text(r)))
+                              .toList(),
+                        ),
+                      );
+                    });
               }
             }));
   }
 }
 
-Widget _buildMissionType(BuildContext context, Jobs job) {
-  return Tiles(
+Widget _buildMissionType(Jobs job, Factionutils futils,
+    OpenWorldFactions faction, BuildContext context) {
+  return Container(
+    height: 80.0,
+    color: futils.buildColor(faction),
+    alignment: Alignment.centerLeft,
     child: ListTile(
-      title: Text(job.type),
-      subtitle:
-          Text('Enemey Level ${job.enemyLevels[0]} - ${job.enemyLevels[1]}'),
-      trailing: ButtonTheme.bar(
-        child: FlatButton(
-            onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                builder: (_) => BountyRewards(
-                      missionTYpe: job.type,
-                      bountyRewards: job.rewardPool.cast<String>(),
-                    ))),
-            child: Text(
-              'See Rewards',
-              style: TextStyle(color: Theme.of(context).accentColor),
-            )),
-      ),
-    ),
+        title: Text(job.type),
+        subtitle:
+            Text('Enemey Level ${job.enemyLevels[0]} - ${job.enemyLevels[1]}'),
+        trailing: Container(
+            child: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          const Icon(Standing.standing),
+          Text(
+              job.standingStages.cast<int>().reduce((a, b) => a + b).toString())
+        ]))),
   );
 }
