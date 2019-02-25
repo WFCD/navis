413f87fd7627e3412bde9f8b051314bd8266d37c
Ludwig <blacklightinglr@gmail.com>
2019-02-24 21:58:35 -0500

    remove shared_pref access from the api

--- lib/services/worldstate.dart
+++ lib/services/worldstate.dart
@@ -1,27 +1,19 @@
 import 'dart:async';
 import 'dart:convert';
+
 import 'package:codable/codable.dart';
-import 'package:http/http.dart';
 import 'package:navis/models/export.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:navis/utils/metric_httpClient.dart';
+import 'package:http/http.dart' as http;
 
 class WorldstateAPI {
   final String _baseRoute = 'https://api.warframestat.us/';
 
-  final MetricHttpClient _metricHttpClient = MetricHttpClient(Client());
-
-  Future<WorldState> updateState() async {
-    final prefs = await SharedPreferences.getInstance();
-    final Request request = Request(
-        'GET', Uri.parse('$_baseRoute${prefs.getString('Platform') ?? 'pc'}'));
-
-    final StreamedResponse response = await _metricHttpClient.send(request);
+  Future<WorldState> updateState(http.Client client, {String platform}) async {
+    final response = await client.get(_baseRoute + platform);
 
     if (response.statusCode != 200) throw Exception('error loading state');
 
-    final data =
-        json.decode(await response.stream.transform(utf8.decoder).join());
+    final data = json.decode(response.body);
 
     final key = KeyedArchive.unarchive(data);
     final WorldState state = WorldState()..decode(key);
@@ -33,7 +25,8 @@ class WorldstateAPI {
   void _cleanState(WorldState state) {
     state.alerts.removeWhere((a) =>
         a.active == false ||
-        a.expiry.difference(DateTime.now().toUtc()) < Duration(seconds: 1));
+        a.expiry.difference(DateTime.now().toUtc()) <
+            const Duration(seconds: 1));
 
     state.news.retainWhere((art) => art.translations.en != null);
     state.news.sort((a, b) => b.date.compareTo(a.date));
@@ -43,14 +36,15 @@ class WorldstateAPI {
 
     state.persistentEnemies.sort((a, b) => a.agentType.compareTo(b.agentType));
 
-    state.syndicates.retainWhere((s) =>
-        s.syndicate.contains(RegExp('(Ostrons)|(Solaris United)')) == true);
+    state.syndicates.retainWhere(
+        (s) => s.name.contains(RegExp('(Ostrons)|(Solaris United)')) == true);
 
-    state.syndicates.sort((a, b) => a.syndicate.compareTo(b.syndicate));
+    state.syndicates.sort((a, b) => a.name.compareTo(b.name));
 
     state.voidFissures.removeWhere((v) =>
         v.active == false ||
-        v.expiry.difference(DateTime.now().toUtc()) < Duration(seconds: 1));
+        v.expiry.difference(DateTime.now().toUtc()) <
+            const Duration(seconds: 1));
 
     state.voidFissures.sort((a, b) => a.tierNum.compareTo(b.tierNum));
   }
