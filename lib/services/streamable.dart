dae6b8e8a1fad371497441cfcc0c2c4f395557c9
Ludwig <blacklightinglr@gmail.com>
2019-02-27 19:36:26 -0500

    remove some uneeeded things

--- lib/services/streamable.dart
+++ lib/services/streamable.dart
@@ -2,15 +2,12 @@ import 'dart:convert';
 import 'package:codable/codable.dart';
 import 'package:http/http.dart' as http;
 
-import 'package:navis/utils/keys.dart';
 import 'package:navis/models/streamable/video.dart';
 
 class StreamableAPI {
   Future<String> fishVideos(String shortCode) async {
-    final data = json.decode((await http.get(
-            'https://api.streamable.com/videos/$shortCode',
-            headers: {'email': streamableUser, 'password': streamablePassword}))
-        .body);
+    final data = json.decode(
+        (await http.get('https://api.streamable.com/videos/$shortCode')).body);
     final key = KeyedArchive.unarchive(data);
     final video = Video()..decode(key);
 
