5723014e901dc0b32ee91d492960d1265373c593
Ludwig <blacklightinglr@gmail.com>
2019-03-03 23:03:29 -0500

    added nightwave model and assets

--- lib/models/export.dart
+++ lib/models/export.dart
@@ -1,14 +1,15 @@
 export 'package:navis/models/worldstate/alerts.dart';
 export 'package:navis/models/worldstate/earth.dart';
-export 'package:navis/models/worldstate/events.dart';
+export 'package:navis/models/worldstate/event.dart';
 export 'package:navis/models/worldstate/orbvallis.dart';
 
+export 'worldstate/fissure.dart';
 export 'worldstate/invasions.dart';
 export 'worldstate/news.dart';
+export 'worldstate/nightwave.dart';
 export 'worldstate/persistentEnemies.dart';
 export 'worldstate/rewards.dart';
 export 'worldstate/sortie.dart';
-export 'worldstate/syndicates.dart';
+export 'worldstate/syndicate.dart';
 export 'worldstate/trader.dart';
-export 'worldstate/void.dart';
 export 'worldstate/worldstate.dart';
