3b9185a2ba92be3b446128b8e2b73654dc412605
Ludwig <blacklightinglr@gmail.com>
2019-03-06 10:06:08 -0500

    temp: removed firebase messaging

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:background_fetch/background_fetch.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+//import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_villains/villain.dart';
