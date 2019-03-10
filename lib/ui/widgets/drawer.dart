0ed68f6e8e6d67441ab2b077c9a5edd11dfd116b
Ludwig <blacklightinglr@gmail.com>
2019-03-10 15:11:55 -0400

    fixed sortie reset crash added aboutTile

--- lib/ui/widgets/drawer.dart
+++ lib/ui/widgets/drawer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:package_info/package_info.dart';
+
 import 'platform_choices.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -20,6 +23,21 @@ class CustomDrawer extends StatelessWidget {
       leading: item.icon,
       title: Text(item.title),
       children: item.children.map(_buildDrawerItem).toList(),
+    );
+  }
+
+  Widget _appIcon() {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(60.0)),
+      child: SvgPicture.asset(
+        'assets/general/nightmare.svg',
+        height: 60,
+        width: 60,
+        color: const Color.fromRGBO(26, 80, 144, .9),
+      ),
     );
   }
 
@@ -46,6 +64,19 @@ class CustomDrawer extends StatelessWidget {
         Navigator.of(context).pushNamed('/Settings');
       },
     ));
+
+    _items.add(FutureBuilder<PackageInfo>(
+        future: PackageInfo.fromPlatform(),
+        builder: (_, snapshot) {
+          return AboutListTile(
+            icon: const Icon(Icons.info),
+            applicationIcon: _appIcon(),
+            applicationName: 'Cephalon Navis',
+            applicationVersion: snapshot.data?.version ?? '',
+            applicationLegalese:
+            'Warframe and the Warframe logo are registered trademarks of Digital Extremes Ltd. Cephalon Navis is not affiliated with Digital Extremes Ltd. in any way.',
+          );
+        }));
 
     return Drawer(
         child: Column(
