296730d15cb57402a6e7b75205cc06955fb1deb9
Ludwig <blacklightinglr@gmail.com>
2019-03-30 01:40:00 -0400

    use official firebase crash plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:navis/utils/crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'app.dart';
 
 void main() {
-  final exception = ExceptionService();
+  bool isDebug = false;
 
-  runZoned<Future<void>>(() async => runApp(Navis()),
-      onError: (error, stackTrace) async =>
-          await exception.reportErrorAndStackTrace(error, stackTrace));
+  assert(isDebug = true);
+  FlutterError.onError = (FlutterErrorDetails details) {
+    if (isDebug) {
+      // In development mode simply print to console.
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      // In production mode report to the application zone to report to
+      // Crashlytics.
+      Crashlytics.instance.onError(details);
+    }
+  };
+
+  runApp(Navis());
 }
