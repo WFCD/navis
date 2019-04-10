7eda565fd4fc391ad55e58a9124b468187108dd3
Ludwig <blacklightinglr@gmail.com>
2019-04-09 23:50:17 -0400

    possible fix for IllegalStateException crash

--- android/app/src/main/kotlin/com/cephalon/navis/MainActivity.kt
+++ android/app/src/main/kotlin/com/cephalon/navis/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.view.FlutterMain
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        FlutterMain.startInitialization(this)
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
     }
