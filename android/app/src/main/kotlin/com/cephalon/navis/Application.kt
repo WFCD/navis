7d106f2e1f8cbefca6acc2d2cb4a6d59d3c0397e
Ludwig <blacklightinglr@gmail.com>
2019-05-08 08:01:27 -0400

    background_fetch: oops

--- android/app/src/main/kotlin/com/cephalon/navis/Application.kt
+++ android/app/src/main/kotlin/com/cephalon/navis/Application.kt
@@ -4,10 +4,12 @@ import com.transistorsoft.flutter.backgroundfetch.BackgroundFetchPlugin;
 import io.flutter.app.FlutterApplication;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.view.FlutterMain
 
 class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
     override fun onCreate() {
         super.onCreate()
+        FlutterMain.startInitialization(this)
         BackgroundFetchPlugin.setPluginRegistrant(this)
     }
 
