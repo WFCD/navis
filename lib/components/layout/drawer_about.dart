b642a3b542e5749d31b4eb8da7f6582a22fd131f
Ludwig <blacklightinglr@gmail.com>
2019-06-16 18:17:16 -0400

    tweaked about dialog

--- lib/components/layout/drawer_about.dart
+++ lib/components/layout/drawer_about.dart
@@ -5,8 +5,27 @@ import 'package:navis/services/services.dart';
 import 'package:navis/utils/link_handler.dart';
 import 'package:package_info/package_info.dart';
 
+const _legalese = 'Warframe and the Warframe logo are registered trademarks '
+    'of Digital Extremes Ltd. Cephalon Navis nor WFCD are '
+    'affiliated with Digital Extremes Ltd. in any way.';
+
 class About extends StatelessWidget {
   const About({Key key}) : super(key: key);
+
+  Widget _appIcon() {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(60.0)),
+      child: SvgPicture.asset(
+        'assets/general/nightmare.svg',
+        color: const Color.fromRGBO(26, 80, 144, .9),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +36,13 @@ class About extends StatelessWidget {
 
     return AboutListTile(
       icon: Icon(Icons.info),
-      applicationIcon: _AppIcon(),
+      applicationIcon: _appIcon(),
       applicationName: 'Cephalon Navis',
       applicationVersion: locator<PackageInfo>().version,
       aboutBoxChildren: <Widget>[
-        const SizedBox(height: 8),
         RichText(
             text: TextSpan(children: <TextSpan>[
-          TextSpan(
-              style: aboutTextStyle,
-              text: 'Warframe and the Warframe logo are registered trademarks '
-                  'of Digital Extremes Ltd. Cephalon Navis is not '
-                  'affiliated with Digital Extremes Ltd. in any way.'),
-          TextSpan(style: aboutTextStyle, text: '\n\nHomepage: '),
+          TextSpan(style: aboutTextStyle, text: 'Homepage: '),
           _LinkTextSpan(
               context: context,
               style: linkStyle,
@@ -56,6 +69,8 @@ class About extends StatelessWidget {
           height: 50,
           child: SvgPicture.asset('assets/general/wfcd_banner.svg'),
         ),
+        const SizedBox(height: 16),
+        Text(_legalese, style: Theme.of(context).textTheme.caption),
       ],
     );
   }
@@ -84,22 +99,4 @@ class _LinkTextSpan extends TextSpan {
               ..onTap = () {
                 launchLink(context, url);
               });
-}
-
-class _AppIcon extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: 50,
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.white),
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(60.0)),
-      child: SvgPicture.asset(
-        'assets/general/nightmare.svg',
-        color: const Color.fromRGBO(26, 80, 144, .9),
-      ),
-    );
-  }
 }
