8f034b7da2b900fe8c5ce41bc3a06a5a26f45901
Ludwig <blacklightinglr@gmail.com>
2019-07-01 02:26:15 -0400

    whoops

--- lib/screens/settings/components/about_app.dart
+++ lib/screens/settings/components/about_app.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:navis/components/layout/about.dart';
+import 'package:navis/components/widgets.dart';
 import 'package:navis/components/layout/setting_title.dart';
+import 'package:navis/services/localstorage_service.dart';
+import 'package:navis/services/services.dart';
 import 'package:navis/utils/link_handler.dart';
 
 class AboutApp extends StatelessWidget {
-  const AboutApp({Key key}) : super(key: key);
+  AboutApp({Key key}) : super(key: key);
+
+  final storage = locator<LocalStorageService>();
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         const SettingTitle(title: 'About'),
+        ListTile(
+          title: const Text('Update Drop Table'),
+          subtitle: Text('Last updated ' + storage.tableTimestamp.toString()),
+        ),
         ListTile(
           title: const Text('Report Issues'),
           subtitle: const Text('Report bugs or Request a feature'),
