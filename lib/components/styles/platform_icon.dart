470b507edb86bf3e1017db9ff223a5724585a944
Ludwig <blacklightinglr@gmail.com>
2019-07-17 09:02:24 -0400

    pubspec: updated deps and platform icons

--- lib/components/styles/platform_icon.dart
+++ lib/components/styles/platform_icon.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:navis/blocs/bloc.dart';
 import 'package:navis/utils/utils.dart';
+import "package:flutter_brand_icons/flutter_brand_icons.dart";
 
 const pc = 'PC';
 const ps4 = 'Sony PlayStation 4';
@@ -14,29 +15,29 @@ class PlatformIcon extends StatelessWidget {
   final Platforms platform;
 
   static String _tooltip;
-  static String _assetPath;
+  static IconData _platformIcon;
   static Color _platformColor;
 
   void _setValues() {
     switch (platform) {
       case Platforms.ps4:
         _tooltip = ps4;
-        _assetPath = 'assets/platforms/ps4.svg';
+        _platformIcon = BrandIcons.playstation;
         _platformColor = const Color(0xFF003791);
         break;
       case Platforms.xb1:
         _tooltip = xb1;
-        _assetPath = 'assets/platforms/xbox1.svg';
-        _platformColor = const Color(0xFF107c10);
+        _platformIcon = BrandIcons.xbox;
+        _platformColor = const Color(0xFF107C10);
         break;
       case Platforms.swi:
         _tooltip = swi;
-        _assetPath = 'assets/platforms/switch.svg';
-        _platformColor = const Color(0xFFe60012);
+        _platformIcon = BrandIcons.nintendoswitch;
+        _platformColor = const Color(0xFFE60012);
         break;
       default:
         _tooltip = pc;
-        _assetPath = 'assets/platforms/pc.svg';
+        _platformIcon = BrandIcons.steam;
         _platformColor = const Color(0xFFFACA04);
     }
   }
@@ -46,7 +47,7 @@ class PlatformIcon extends StatelessWidget {
     final state = BlocProvider.of<WorldstateBloc>(context);
     final storage = BlocProvider.of<StorageBloc>(context);
 
-    void _onPressed(Platforms platforms) {
+    void _onPressed() {
       storage.dispatch(ChangePlatformEvent(platform));
       state.dispatch(UpdateEvent.update);
       //Navigator.of(context).pop();
@@ -56,19 +57,16 @@ class PlatformIcon extends StatelessWidget {
       bloc: storage,
       builder: (_, state) {
         _setValues();
-
         return IconButton(
           tooltip: _tooltip,
-          icon: SvgPicture.asset(
-            _assetPath,
+          iconSize: 30,
+          icon: Icon(
+            _platformIcon,
             color: state.platform == platform
                 ? _platformColor
                 : Theme.of(context).disabledColor,
-            height: 25,
-            width: 25,
-            semanticsLabel: _tooltip,
           ),
-          onPressed: () => _onPressed(Platforms.pc),
+          onPressed: _onPressed,
         );
       },
     );
