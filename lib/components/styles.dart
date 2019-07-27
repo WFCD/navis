65e3503499b46225fbd6fd39fa70d2426e61baa0
Ludwig <blacklightinglr@gmail.com>
2019-07-26 23:46:51 -0400

    removed deprecated invasion style and bar

--- lib/components/styles.dart
+++ lib/components/styles.dart
@@ -1,5 +1,3 @@
-export 'styles/invasionsBar.dart';
-export 'styles/invasions_style.dart';
 export 'styles/platform_choices.dart';
 export 'styles/platform_icon.dart';
 export 'styles/timer_style.dart';
