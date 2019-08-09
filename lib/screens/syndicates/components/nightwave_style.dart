b4c017d4e7693d123dd817b4ff056ff3734cd538
Ludwig <blacklightinglr@gmail.com>
2019-08-09 02:58:17 -0400

    fix nightwave typo

--- lib/screens/syndicates/components/nightwave_style.dart
+++ lib/screens/syndicates/components/nightwave_style.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navis/components/layout.dart';
 
 import '../nightwaveChallenges.dart';
 
@@ -12,37 +13,34 @@ class NightWaveStyle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 6,
-      margin: const EdgeInsets.all(6.0),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      child: Container(
-          constraints: const BoxConstraints.expand(height: 100.0),
-          height: 100,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-              image: DecorationImage(
-                  image: const AssetImage('assets/general/banner.webp'),
-                  fit: BoxFit.cover)),
-          child: InkWell(
-              onTap: () => _navigateToChallenges(context),
-              child: Container(
-                  height: 40,
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                      color: Color.fromRGBO(34, 34, 34, .2)),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text('NIghtwave',
-                            style: Theme.of(context)
-                                .textTheme
-                                .title
-                                .copyWith(color: Colors.white, fontSize: 20)),
-                        Text('Season ${season + 1}',
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context).textTheme.subtitle)
-                      ])))),
+    return BackgroundImageCard(
+      height: 100,
+      provider: const AssetImage('assets/general/banner.webp'),
+      child: InkWell(
+        onTap: () => _navigateToChallenges(context),
+        child: Container(
+          height: 40,
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            color: Color.fromRGBO(34, 34, 34, .2),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Nightwave',
+                  style: Theme.of(context).textTheme.title.copyWith(
+                        color: Colors.white,
+                        fontSize: 20,
+                      )),
+              Text(
+                'Season $season',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.subtitle,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
