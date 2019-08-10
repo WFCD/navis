5a4a101d7de92ca0ea60488bda5cf1226dc57975
Ludwig <blacklightinglr@gmail.com>
2019-08-10 16:09:25 -0400

    add pageview widget the return of invasion custom painter

---
+++ lib/components/widgets/carousel.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:navis/components/layout.dart';
+import 'package:rxdart/rxdart.dart';
+
+class Carousel extends StatefulWidget {
+  const Carousel({
+    Key key,
+    @required this.children,
+    @required this.dotCount,
+    this.height = 150,
+    this.enableIndicator = true,
+  })  : assert(children != null),
+        assert(dotCount != null),
+        super(key: key);
+
+  final List<Widget> children;
+  final double height;
+  final int dotCount;
+  final bool enableIndicator;
+
+  @override
+  _CarouselState createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+  double _page;
+  StreamController<int> _currentPage;
+  PageController _pageController;
+  PageStorageBucket _bucket;
+
+  @override
+  void initState() {
+    super.initState();
+    _bucket = PageStorage.of(context);
+    _page = _bucket.readState(context);
+
+    _pageController = PageController(initialPage: _page?.toInt() ?? 0);
+
+    _currentPage = BehaviorSubject<int>();
+
+    _currentPage.sink.add(_page?.toInt() ?? 0);
+  }
+
+  @override
+  void dispose() {
+    _currentPage?.close();
+    _pageController?.dispose();
+    super.dispose();
+  }
+
+  void onPageChanged(int index) {
+    _currentPage.sink.add(index);
+    _bucket.writeState(context, index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: widget.height + 15,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: widget.height,
+            child: PageView(
+              controller: _pageController,
+              children: widget.children,
+              onPageChanged: onPageChanged,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              // color: const Color.fromRGBO(34, 34, 34, .4),
+              height: 20,
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.bottomCenter,
+              child: StreamBuilder<int>(
+                stream: _currentPage.stream,
+                initialData: _page?.toInt() ?? 0,
+                builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                  return Indicator(
+                    numberOfDot: widget.dotCount,
+                    position: snapshot.data,
+                    dotSize: const Size.square(9.0),
+                    dotActiveColor: Theme.of(context).accentColor,
+                  );
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/widgets/invasions_bar.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import 'package:navis/utils/factionutils.dart';
+
+class InvasionBar extends StatelessWidget {
+  const InvasionBar({
+    Key key,
+    @required this.progress,
+    this.width,
+    this.padding,
+    @required this.attackingFaction,
+    @required this.defendingFaction,
+    this.color,
+    this.lineHeight,
+  })  : assert(attackingFaction != null),
+        assert(defendingFaction != null),
+        assert(progress != null),
+        super(key: key);
+
+  final String attackingFaction, defendingFaction;
+  final double progress;
+  final double width;
+  final double lineHeight;
+  final EdgeInsets padding;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 6.0,
+      color: Colors.transparent,
+      clipBehavior: Clip.hardEdge,
+      borderRadius: BorderRadius.circular(16.0),
+      child: Row(children: <Widget>[
+        Expanded(
+          child: Container(
+            height: lineHeight * 2,
+            width: width,
+            padding: padding,
+            child: CustomPaint(
+              willChange: true,
+              painter: _InvasionBar(
+                progress: progress,
+                progressColor: factionColor(attackingFaction),
+                backgroundColor: factionColor(defendingFaction),
+                lineWidth: lineHeight,
+              ),
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
+
+class _InvasionBar extends CustomPainter {
+  _InvasionBar(
+      {this.progress,
+      this.progressColor,
+      this.backgroundColor,
+      this.lineWidth}) {
+    _attackingFaction.color = progressColor;
+    _attackingFaction.style = PaintingStyle.stroke;
+    _attackingFaction.strokeWidth = lineWidth;
+    _attackingFaction.strokeCap = StrokeCap.butt;
+
+    _defendingFaction.color = backgroundColor;
+    _defendingFaction.style = PaintingStyle.stroke;
+    _defendingFaction.strokeWidth = lineWidth;
+    _defendingFaction.strokeCap = StrokeCap.butt;
+  }
+
+  final Paint _attackingFaction = Paint();
+  final Paint _defendingFaction = Paint();
+  final Color backgroundColor;
+  final Color progressColor;
+  final double progress;
+  final double lineWidth;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Offset start = Offset(0.0, size.height / 2);
+    final Offset end = Offset(size.width, size.height / 2);
+
+    canvas.drawLine(start, end, _defendingFaction);
+    canvas.drawLine(start, Offset(size.width * progress, size.height / 2),
+        _attackingFaction);
+  }
+
+  @override
+  bool shouldRebuildSemantics(_InvasionBar oldDelegate) =>
+      oldDelegate.progress != progress;
+
+  @override
+  bool shouldRepaint(_InvasionBar oldDelegate) =>
+      oldDelegate.progress != progress;
+}
