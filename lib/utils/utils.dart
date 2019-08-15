92fa354c56823428de069a99b3f45f449b765756
Ludwig <blacklightinglr@gmail.com>
2019-08-15 03:18:26 -0400

    cleaned up linklauncher

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -10,31 +10,35 @@ enum Formats { mm_dd_yy, dd_mm_yy, month_day_year }
 Future<void> launchLink(BuildContext context, String link,
     {bool isStream = false}) async {
   const String warframeTwitch = 'twitch://open?stream=warframe';
-  try {
+  if (await canLaunch(link)) {
     if (isStream) {
-      if (await canLaunch(warframeTwitch))
-        await launch('twitch://open?stream=warframe');
-      else
+      if (await canLaunch(warframeTwitch)) {
+        await launch(warframeTwitch);
+      } else {
         launchLink(context, link, isStream: false);
-    } else
+      }
+    } else {
       await custom.launch(link,
           option: custom.CustomTabsOption(
-              toolbarColor: Theme.of(context).primaryColor,
-              enableDefaultShare: true,
-              enableUrlBarHiding: true,
-              showPageTitle: true,
-              animation: custom.CustomTabsAnimation.slideIn(),
-              extraCustomTabs: const <String>[
-                'org.mozilla.firefox',
-                'org.mozilla.fenix',
-                'com.microsoft.emmx'
-              ]));
-  } catch (err) {
+            toolbarColor: Theme.of(context).primaryColor,
+            enableDefaultShare: true,
+            enableUrlBarHiding: true,
+            showPageTitle: true,
+            animation: custom.CustomTabsAnimation.slideIn(),
+            extraCustomTabs: const <String>[
+              'org.mozilla.firefox',
+              'org.mozilla.fenix',
+              'com.microsoft.emmx'
+            ],
+          ));
+    }
+  } else {
     scaffold.currentState.showSnackBar(SnackBar(
-        duration: const Duration(seconds: 5),
-        content: link.isEmpty
-            ? const Text('No valid link provided by API')
-            : const Text('No App compatible detected to open link')));
+      duration: const Duration(seconds: 5),
+      content: link.isEmpty
+          ? const Text('No valid link provided by API')
+          : const Text('No App compatible detected to open link'),
+    ));
   }
 }
 
