0d61882771c83156df4c69ddec6eeb09695de0ea
Ludwig <blacklightinglr@gmail.com>
2019-08-15 04:03:40 -0400

    remove offline drop table
    no point in it, if the user doesn't have internet it will download next
    time the app is opened.
    just a matter of telling the user it
    wasn;t downloaded

--- lib/services/repository.dart
+++ lib/services/repository.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:navis/models/drop_table_info.dart';
