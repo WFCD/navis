27039fd75c74283e427128ea3088b73fbd6d5ca8
Ludwig <blacklightinglr@gmail.com>
2019-08-19 09:14:48 -0400

    cycles: refactor to use facotories

--- lib/screens/home/components/cycles.dart
+++ lib/screens/home/components/cycles.dart
@@ -1,76 +1,56 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:navis/blocs/bloc.dart';
-import 'package:navis/components/layout/cycles_base.dart';
-import 'package:navis/utils/worldstate_utils.dart';
+import 'package:intl/intl.dart';
+import 'package:navis/components/animations.dart';
+import 'package:navis/components/layout.dart';
+import 'package:navis/components/widgets/date.dart';
+import 'package:worldstate_model/worldstate_models.dart';
+import 'package:worldstate_model/worldstate_objects.dart';
 
-const List<Widget> cycles = [CetusCycle(), EarthCycle(), OrbVallisCycle()];
+class Cycles extends StatelessWidget {
+  const Cycles({
+    @required this.title,
+    @required this.cycle,
+  })  : assert(title != null),
+        assert(cycle != null);
 
-class EarthCycle extends StatelessWidget {
-  const EarthCycle({Key key}) : super(key: key);
+  factory Cycles.cetus(Cetus cetus) =>
+      Cycles(title: 'Cetus Day/Night Cycle', cycle: cetus);
 
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<WorldstateBloc, WorldStates>(
-      bloc: BlocProvider.of<WorldstateBloc>(context),
-      condition: (WorldStates previous, WorldStates current) {
-        return compareExpiry(
-          previous: previous.worldstate.earthCycle?.expiry,
-          current: current.worldstate.earthCycle?.expiry,
-        );
-      },
-      builder: (BuildContext context, WorldStates state) {
-        return Cycle(
-          title: 'Earth Day/Night Cycle',
-          cycle: state.worldstate?.earthCycle,
-        );
-      },
-    );
-  }
-}
+  factory Cycles.orbValis(Vallis orbVallis) =>
+      Cycles(title: 'Orb Vallis Warm/Cold Cycle', cycle: orbVallis);
 
-class CetusCycle extends StatelessWidget {
-  const CetusCycle({Key key}) : super(key: key);
+  factory Cycles.earth(Earth earth) =>
+      Cycles(title: 'Earth Day/Night Cycle', cycle: earth);
+
+  final String title;
+  final CycleObject cycle;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<WorldstateBloc, WorldStates>(
-      bloc: BlocProvider.of<WorldstateBloc>(context),
-      condition: (WorldStates previous, WorldStates current) {
-        return compareExpiry(
-          previous: previous.worldstate.cetusCycle?.expiry,
-          current: current.worldstate.cetusCycle?.expiry,
-        );
-      },
-      builder: (BuildContext context, WorldStates state) {
-        return Cycle(
-          title: 'Cetus Day/Night Cycle',
-          cycle: state.worldstate?.cetusCycle,
-        );
-      },
-    );
-  }
-}
+    const padding = SizedBox(height: 8.0);
 
-class OrbVallisCycle extends StatelessWidget {
-  const OrbVallisCycle({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<WorldstateBloc, WorldStates>(
-      bloc: BlocProvider.of<WorldstateBloc>(context),
-      condition: (WorldStates previous, WorldStates current) {
-        return compareExpiry(
-          previous: previous.worldstate.vallisCycle?.expiry,
-          current: current.worldstate.vallisCycle?.expiry,
-        );
-      },
-      builder: (BuildContext context, WorldStates state) {
-        return Cycle(
-          title: 'Orb Vallis Warm/Cold Cycle',
-          cycle: state.worldstate?.vallisCycle,
-        );
-      },
-    );
+    return Tiles(
+        title: title,
+        child: Column(
+          children: <Widget>[
+            RowItem.richText(
+              title: 'Currently it is',
+              richText: toBeginningOfSentenceCase(cycle.state),
+              color: cycle.getStateBool ? Colors.yellow[700] : Colors.blue,
+              size: 20.0,
+            ),
+            padding,
+            RowItem(
+              text: 'Time until ${cycle.nextState}',
+              child: CountdownBox(expiry: cycle.expiry),
+            ),
+            padding,
+            RowItem(
+              text: 'Time at ${cycle.nextState}',
+              child: DateView(expiry: cycle.expiry),
+            ),
+            padding
+          ],
+        ));
   }
 }
