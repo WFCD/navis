346f9da394b286b9754ea3dea94002718c40f028
Ludwig <blacklightinglr@gmail.com>
2019-08-21 10:36:22 -0400

    nightwave: wrap with SafeArea

--- lib/screens/syndicates/nightwaveChallenges.dart
+++ lib/screens/syndicates/nightwaveChallenges.dart
@@ -12,37 +12,41 @@ class NightwaveChallenges extends StatelessWidget {
   Widget build(BuildContext context) {
     final style = Theme.of(context).textTheme.subtitle.copyWith(fontSize: 15);
 
-    return Scaffold(
-        appBar: AppBar(
-            title: const Text('Nightwave'), backgroundColor: Colors.red[800]),
-        body: BlocBuilder(
-          bloc: BlocProvider.of<WorldstateBloc>(context),
-          builder: (_, state) {
-            if (state is WorldstateLoaded) {
-              const padding = SizedBox(height: 8.0);
-              final nightwave = state.worldstate.nightwave;
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Nightwave'),
+            backgroundColor: Colors.red[800],
+          ),
+          body: BlocBuilder(
+            bloc: BlocProvider.of<WorldstateBloc>(context),
+            builder: (_, state) {
+              if (state is WorldstateLoaded) {
+                const padding = SizedBox(height: 8.0);
+                final nightwave = state.worldstate.nightwave;
 
-              final daily = nightwave
-                  .dailyChallenges()
-                  .map((c) => ChallengeType(challenge: c));
+                final daily = nightwave
+                    .dailyChallenges()
+                    .map((c) => ChallengeType(challenge: c));
 
-              final weekly = nightwave
-                  .weeklyChallenges()
-                  .map((c) => ChallengeType(challenge: c));
+                final weekly = nightwave
+                    .weeklyChallenges()
+                    .map((c) => ChallengeType(challenge: c));
 
-              return ListView(children: <Widget>[
-                padding,
-                SettingTitle(title: 'Daily', style: style),
-                ...daily,
-                padding,
-                SettingTitle(title: 'Weekly', style: style),
-                ...weekly
-              ]);
-            }
+                return ListView(children: <Widget>[
+                  padding,
+                  SettingTitle(title: 'Daily', style: style),
+                  ...daily,
+                  padding,
+                  SettingTitle(title: 'Weekly', style: style),
+                  ...weekly
+                ]);
+              }
 
-            return Container();
-          },
-        ));
+              return Container();
+            },
+          )),
+    );
   }
 }
 
@@ -72,30 +76,32 @@ class ChallengeType extends StatelessWidget {
         Theme.of(context).textTheme.body1.copyWith(fontSize: 14, color: color);
 
     return Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        child: Container(
-            margin: const EdgeInsets.all(8.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Container(child: Text(challenge.title, style: title)),
-                  const SizedBox(height: 4.0),
-                  Container(child: Text(challenge.desc, style: desscription)),
-                  const SizedBox(height: 8.0),
-                  Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        if (challenge?.isElite)
-                          StaticBox.text(
-                            color: Colors.red,
-                            text: 'Elite',
-                            size: 14,
-                          ),
-                        _standingBadge(),
-                        CountdownBox(expiry: challenge.expiry)
-                      ]),
-                ])));
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(child: Text(challenge.title, style: title)),
+              const SizedBox(height: 4.0),
+              Container(child: Text(challenge.desc, style: desscription)),
+              const SizedBox(height: 8.0),
+              Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    if (challenge?.isElite)
+                      StaticBox.text(
+                        color: Colors.red,
+                        text: 'Elite',
+                        size: 14,
+                      ),
+                    _standingBadge(),
+                    CountdownBox(expiry: challenge.expiry)
+                  ]),
+            ]),
+      ),
+    );
   }
 }
