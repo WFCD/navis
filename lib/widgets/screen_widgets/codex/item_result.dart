afd68d096808bff97692e324acba986b3863b837
Ludwig <blacklightinglr@gmail.com>
2019-09-08 22:28:10 -0400

    remove material layer

--- lib/widgets/screen_widgets/codex/item_result.dart
+++ lib/widgets/screen_widgets/codex/item_result.dart
@@ -1,6 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:navis/utils/utils.dart';
-import 'package:navis/widgets/screen_widgets/items_info/item_image.dart';
 import 'package:warframe_items_model/warframe_items_model.dart';
 
 class ItemResultWidget extends StatelessWidget {
@@ -10,10 +10,20 @@ class ItemResultWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const height = 100.0;
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 8.0),
       child: ListTile(
-        leading: ItemImage(imageName: item.imageName),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(4.0),
+          child: CachedNetworkImage(
+            imageUrl: 'https://cdn.warframestat.us/img/${item.imageName}',
+            fit: BoxFit.cover,
+            width: (1.5 * height) / 2,
+            height: height,
+          ),
+        ),
         title: Text(item.name),
         subtitle: Text(parseHtmlString(item.description ?? '')),
         dense: true,
