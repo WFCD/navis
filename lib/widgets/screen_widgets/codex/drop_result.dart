95c4418991124470cd833769273223418e204b28
Ludwig <blacklightinglr@gmail.com>
2019-09-19 22:32:28 -0400

    remove drop chance color
    yeah yellow and that 'silver' weren't working so great with light mode

--- lib/widgets/screen_widgets/codex/drop_result.dart
+++ lib/widgets/screen_widgets/codex/drop_result.dart
@@ -7,34 +7,19 @@ class DropResultWidget extends StatelessWidget {
 
   final SlimDrop drop;
 
-  Color _chance(num chance) {
-    if (chance < 25) return Colors.yellow;
-    if (chance > 25 && chance < 50) return Colors.white;
+  // Color _chance(num chance) {
+  //   if (chance < 25) return Colors.yellow;
+  //   if (chance > 25 && chance < 50) return Colors.white;
 
-    return Colors.brown;
-  }
+  //   return Colors.brown;
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
     return ListTile(
       title: Text(drop.item),
-      subtitle: RichText(
-        text: TextSpan(children: [
-          TextSpan(
-            text: parseHtmlString(drop.place),
-            style: theme.textTheme.caption,
-          ),
-          const TextSpan(text: ' | '),
-          TextSpan(
-            text: '${drop.chance}% Drop Chance',
-            style: theme.textTheme.caption.copyWith(
-              color: _chance(drop.chance),
-            ),
-          ),
-        ]),
-      ),
+      subtitle:
+          Text('${parseHtmlString(drop.place)} | ${drop.chance}% Drop Chance'),
       dense: true,
     );
   }
