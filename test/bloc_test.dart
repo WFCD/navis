80282bc4011b664024bf36df3e09e5c33aa5e752
Ludwig <blacklightinglr@gmail.com>
2019-10-22 19:50:28 -0400

    whoops left some ununsed imports

--- test/bloc_test.dart
+++ test/bloc_test.dart
@@ -3,12 +3,9 @@ import 'dart:io';
 
 import 'package:bloc/bloc.dart';
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:mockito/mockito.dart';
 import 'package:navis/blocs/bloc.dart';
-import 'package:warframe_items_model/warframe_items_model.dart';
 import 'package:worldstate_model/worldstate_models.dart';
 
 import 'mocked_classes.dart';
@@ -17,9 +14,7 @@ import 'setup_methods.dart';
 void main() {
   final worldstateJson = File('test/worldstate.json').readAsStringSync();
   final worldstate = Worldstate.fromJson(json.decode(worldstateJson));
-  final api = MockWorldstateService();
 
-  SearchBloc searchBloc;
   WorldstateBloc worldstateBloc;
 
   setUpAll(() async {
@@ -27,7 +22,6 @@ void main() {
 
     BlocSupervisor.delegate = await HydratedBlocDelegate.build();
 
-    searchBloc = MockSearchBloc();
     worldstateBloc = MockWorldstateBloc();
   });
 
