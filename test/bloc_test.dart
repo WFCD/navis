2d690ab88b427e771a99198d6e5d1597a43b1794
Ludwig <blacklightinglr@gmail.com>
2019-10-22 19:46:22 -0400

    rewrote worldstateBloc test

--- test/bloc_test.dart
+++ test/bloc_test.dart
@@ -1,133 +1,48 @@
 import 'dart:convert';
-import 'dart:io'; //ignore: unused_import
+import 'dart:io';
 
 import 'package:bloc/bloc.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mockito/mockito.dart';
 import 'package:navis/blocs/bloc.dart';
-import 'package:navis/blocs/worldstate/worldstate_events.dart';
-import 'package:test/test.dart';
-import 'package:wfcd_api_wrapper/worldstate_wrapper.dart';
+import 'package:warframe_items_model/warframe_items_model.dart';
 import 'package:worldstate_model/worldstate_models.dart';
 
 import 'mocked_classes.dart';
 import 'setup_methods.dart';
 
-Future<void> main() async {
-  final mockWorldstateService = MockWorldstateService();
-  // final mockLocalStorageService = MockLocalStrorageService();
-  // final mockNotificationService = MockNotificationService();
+void main() {
+  final worldstateJson = File('test/worldstate.json').readAsStringSync();
+  final worldstate = Worldstate.fromJson(json.decode(worldstateJson));
+  final api = MockWorldstateService();
 
-  // Repository repository;
-  WorldstateBloc worldstate;
-  // Box prefs;
+  SearchBloc searchBloc;
+  WorldstateBloc worldstateBloc;
 
   setUpAll(() async {
     await setupPackageMockMethods();
 
     BlocSupervisor.delegate = await HydratedBlocDelegate.build();
 
-    // repository = await Repository.initRepository();
-
-    worldstate = WorldstateBloc(mockWorldstateService);
-    // prefs = await Hive.openBox('settings');
+    searchBloc = MockSearchBloc();
+    worldstateBloc = MockWorldstateBloc();
   });
 
-  group('Test Worldstate bloc', () {
-    test('Initial state is Worldstate Uninitialized', () {
-      expect(worldstate.initialState, WorldstateUninitialized());
+  group('WorldstateBloc group', () {
+    test('Enusre that Worldstate is Loaded', () async {
+      whenListen(
+          worldstateBloc, Stream.fromIterable([WorldstateLoaded(worldstate)]));
+
+      expectLater(worldstateBloc,
+          emitsInOrder(<WorldStates>[WorldstateLoaded(worldstate)]));
     });
 
-    test('Worldstate is loaded correctly', () async {
-      when(mockWorldstateService.getWorldstate(Platforms.pc))
-          .thenAnswer((_) async {
-        final data = json.decode(File('worldstate.json').readAsStringSync());
-
-        return Worldstate.fromJson(data);
-      });
-
-      worldstate.add(UpdateEvent());
-
-      expectLater(worldstate.state,
-          emitsThrough(const TypeMatcher<WorldstateLoaded>()));
-    });
-
-    test('dispose does not create a new state', () {
-      expectLater(worldstate.state, emitsInOrder([]));
-
-      worldstate.close();
+    test('Make sure nothing is emited after closing', () {
+      worldstateBloc.close();
+      expectLater(worldstateBloc, emitsInOrder([emitsDone]));
     });
   });
-
-  // group('Storage bloc related test', () {
-  //   test('Test dateformat dd_mm_yy', () async {
-  //     final format = await testDateformat(storage, Formats.dd_mm_yy, prefs);
-
-  //     expect(format, Formats.dd_mm_yy);
-  //   });
-
-  //   test('Test dateformat mm_dd_yy', () async {
-  //     final format = await testDateformat(storage, Formats.mm_dd_yy, prefs);
-
-  //     expect(format, Formats.mm_dd_yy);
-  //   });
-
-  //   test('Test dateformat month_day_year', () async {
-  //     final format =
-  //         await testDateformat(storage, Formats.month_day_year, prefs);
-
-  //     expect(format, Formats.month_day_year);
-  //   });
-
-  //   // The default is pc so just need to make sure any other platform can be saved.
-  //   test('Nintendo Switch', () async {
-  //     final storedPlatform = await testStorage(storage, Platforms.swi, prefs);
-
-  //     expect(storedPlatform, Platforms.swi);
-  //   });
-
-  //   test('Playstation 4', () async {
-  //     final storedPlatform = await testStorage(storage, Platforms.ps4, prefs);
-
-  //     expect(storedPlatform, Platforms.ps4);
-  //   });
-
-  //   test('Xbox one', () async {
-  //     final storedPlatform = await testStorage(storage, Platforms.xb1, prefs);
-
-  //     expect(storedPlatform, Platforms.xb1);
-  //   });
-
-  //   test('PC', () async {
-  //     final storedPlatform = await testStorage(storage, Platforms.pc, prefs);
-
-  //     expect(storedPlatform, Platforms.pc);
-  //   });
-  // });
 }
-
-// Future<Platforms> testStorage(
-//   StorageBloc storage,
-//   Platforms platform,
-//   Box prefs,
-// ) async {
-//   storage.dispatch(ChangePlatformEvent(platform));
-
-//   await Future.delayed(const Duration(milliseconds: 500));
-
-//   return Platforms.values.firstWhere((p) =>
-//       p.toString() == 'Platforms.${prefs.get(SettingsKeys.platformKey)}');
-// }
-
-// Future<Formats> testDateformat(
-//   StorageBloc storage,
-//   Formats format,
-//   Box prefs,
-// ) async {
-//   storage.dispatch(ChangeDateFormat(format));
-
-//   await Future.delayed(const Duration(milliseconds: 500));
-
-//   return Formats.values.firstWhere((f) =>
-//       f.toString() == 'Formats.${prefs.get(SettingsKeys.dateformatKey)}');
-// }
--- test/mocked_classes.dart
+++ test/mocked_classes.dart
@@ -1,5 +1,7 @@
+import 'package:hive/hive.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
+import 'package:navis/blocs/bloc.dart';
 import 'package:navis/services/localstorage_service.dart';
 import 'package:navis/services/notification_service.dart';
 import 'package:navis/services/worldstate_service.dart';
@@ -11,3 +13,7 @@ class MockWorldstateService extends Mock implements WorldstateService {}
 class MockLocalStrorageService extends Mock implements LocalStorageService {}
 
 class MockNotificationService extends Mock implements NotificationService {}
+
+class MockWorldstateBloc extends Mock implements WorldstateBloc {}
+
+class MockSearchBloc extends Mock implements SearchBloc {}
