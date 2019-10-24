517cefa8cfb0125ee78fd5e1e4353326bd97ffcd
Ludwig <blacklightinglr@gmail.com>
2019-10-23 21:22:58 -0400

    invasions: need more room

--- lib/widgets/screen_widgets/invasions/invasions_widget.dart
+++ lib/widgets/screen_widgets/invasions/invasions_widget.dart
@@ -14,7 +14,7 @@ class InvasionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SkyboxCard(
-      height: 160,
+      height: 200,
       elevation: 6.0,
       node: invasion.node,
       child: Padding(
