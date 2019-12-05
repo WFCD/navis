388d624796c7877703bce691fc95210ec549650a
Ludwig <blacklightinglr@gmail.com>
2019-12-05 07:52:39 -0500

    remove twitch link launcher

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,43 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_custom_tabs/flutter_custom_tabs.dart' as custom;
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 import 'package:html/parser.dart';
 import 'package:navis/utils/search_utils.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../global_keys.dart';
 
-Future<void> launchLink(BuildContext context, String link,
-    {bool isStream = false}) async {
-  const String warframeTwitch = 'twitch://open?stream=warframe';
-  if (await canLaunch(link)) {
-    if (isStream) {
-      if (await canLaunch(warframeTwitch)) {
-        await launch(warframeTwitch);
-      } else {
-        launchLink(context, link, isStream: false);
-      }
-    } else {
-      await custom.launch(link,
-          option: custom.CustomTabsOption(
-            toolbarColor: Theme.of(context).primaryColor,
-            enableDefaultShare: true,
-            enableUrlBarHiding: true,
-            showPageTitle: true,
-            animation: custom.CustomTabsAnimation.slideIn(),
-            extraCustomTabs: const <String>[
-              'org.mozilla.firefox',
-              'org.mozilla.fenix',
-              'com.microsoft.emmx'
-            ],
-          ));
-    }
-  } else {
-    appScaffold.currentState.showSnackBar(SnackBar(
-      duration: const Duration(seconds: 5),
-      content: link?.isEmpty ?? true
-          ? const Text('No valid link provided by API')
-          : const Text('No App compatible detected to open link'),
+Future<void> launchLink(BuildContext context, String link) async {
+  try {
+    await launch(link,
+        option: CustomTabsOption(
+          toolbarColor: Theme.of(context).primaryColor,
+          enableDefaultShare: true,
+          enableUrlBarHiding: true,
+          showPageTitle: true,
+          animation: CustomTabsAnimation.slideIn(),
+          extraCustomTabs: const <String>[
+            'org.mozilla.firefox',
+            'org.mozilla.fenix',
+            'com.microsoft.emmx'
+          ],
+        ));
+  } catch (e) {
+    appScaffold.currentState.showSnackBar(const SnackBar(
+      duration: Duration(seconds: 5),
+      content: Text('No valid link provided by API'),
     ));
   }
 }
