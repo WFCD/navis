ff8ddac4adde16d282d55e1d53c80a5885a390b2
SlayerOrnstein <blacklightinglr@gmail.com>
2019-12-30 18:29:07 -0500

    import wrappers

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,15 @@ import 'package:bloc/bloc.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:navis/app.dart';
 import 'package:path_provider/path_provider.dart';
 
-import 'repository/repositories.dart';
 import 'resources/storage/cache.dart';
 import 'resources/storage/persistent.dart';
+import 'widgets/wrappers/bloc_wrapper.dart';
+import 'widgets/wrappers/repository_wrapper.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,17 +32,10 @@ Future<void> main() async {
 
   runZoned<void>(() {
     runApp(
-      MultiRepositoryProvider(
-        providers: <RepositoryProvider>[
-          RepositoryProvider<PersistentResource>.value(value: persistent),
-          RepositoryProvider<CacheResource>.value(value: cache),
-          RepositoryProvider<WorldstateRepository>(
-            create: (_) => const WorldstateRepository(),
-          ),
-          RepositoryProvider<DropTableRepository>(
-              create: (_) => DropTableRepository())
-        ],
-        child: const NavisApp(),
+      RepositoryWrapper(
+        persistentResource: persistent,
+        cacheResource: cache,
+        child: const BlocWrapper(child: NavisApp()),
       ),
     );
   }, onError: Crashlytics.instance.recordError);
