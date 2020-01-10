ef37107d73a77ffcb2e38d65f440089baa6a67ed
SlayerOrnstein <blacklightinglr@gmail.com>
2020-01-09 22:06:47 -0500

    bring in repository changes

---
+++ lib/repository/drop_table_repository.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:navis/resources/api/drop_table_client.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:warframe_items_model/warframe_items_model.dart';
+
+class DropTableRepository {
+  DropTableRepository();
+
+  List<SlimDrop> table;
+
+  static const DropTableClient _dropTableProvider = DropTableClient();
+
+  Future<DateTime> get timestamp => _dropTableProvider.dropsTimestamp();
+
+  Future<void> initDrops() async {
+    final table = await _dropTablePath();
+
+    if (table.existsSync()) return;
+
+    await compute<File, void>(_downloadDrops, table);
+
+    this.table = await compute(_toDrops, table.readAsStringSync());
+  }
+
+  Future<List<SlimDrop>> search(String term) async {
+    if (table != null) {
+      final instance = DropSearchInstance(term, table);
+
+      return compute(_searcher, instance);
+    }
+
+    return null;
+  }
+
+  Future<DateTime> updateDrops(DateTime timestamp) async {
+    final _timestamp = await this.timestamp;
+    final table = await _dropTablePath();
+
+    if (timestamp != _timestamp) {
+      await compute<File, void>(_downloadDrops, table);
+      return _timestamp;
+    }
+
+    return timestamp;
+  }
+
+  Future<File> _dropTablePath() async {
+    final temp = await getTemporaryDirectory();
+    final table = File('${temp.path}/drop_table.json');
+
+    return table;
+  }
+
+  static Future<List<SlimDrop>> _toDrops(String table) async {
+    final _table = json.decode(table) as List<Map<String, dynamic>>;
+
+    return _table.map<SlimDrop>((d) => SlimDrop.fromJson(d)).toList();
+  }
+
+  static Future<void> _downloadDrops(File table) async {
+    const DropTableClient _dropTableProvider = DropTableClient();
+    await _dropTableProvider.downloadDropTable(table);
+  }
+
+  static List<SlimDrop> _searcher(DropSearchInstance instance) {
+    return instance.drops.where((d) => d.item.contains(instance.term)).toList();
+  }
+}
+
+class DropSearchInstance {
+  final String term;
+
+  final List<SlimDrop> drops;
+  const DropSearchInstance(this.term, this.drops);
+}
---
+++ lib/repository/repositories.dart
@@ -0,0 +1,3 @@
+export 'drop_table_repository.dart';
+export 'notification_repository.dart';
+export 'worldstate_repository.dart';
