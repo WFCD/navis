2489f78b4d5a6ec587a368c4fe1b2d55f9aa651a
SlayerOrnstein <blacklightinglr@gmail.com>
2020-01-09 19:47:00 -0500

    polished outpost panel

--- lib/widgets/home/sentient_outpost.dart
+++ lib/widgets/home/sentient_outpost.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:navis/utils/size_config.dart';
+import 'package:navis/widgets/common/skybox_card.dart';
+import 'package:navis/widgets/icons.dart';
 import 'package:worldstate_api_model/worldstate_models.dart';
-
-import '../common/cards.dart';
 
 class SentientOutpostPanel extends StatelessWidget {
   const SentientOutpostPanel({Key key, this.outpost}) : super(key: key);
@@ -12,21 +13,40 @@ class SentientOutpostPanel extends StatelessWidget {
   Widget build(BuildContext context) {
     final mission = outpost.mission;
 
+    final fontSize = SizeConfig.textMultiplier * 5;
+
     final node = Theme.of(context)
         .textTheme
         .subhead
-        .copyWith(fontWeight: FontWeight.w700);
+        .copyWith(color: Colors.white, fontSize: fontSize);
 
-    return Tiles(
-      title: 'Sentient Outpost',
-      child: RichText(
-        text: TextSpan(children: <TextSpan>[
-          TextSpan(text: '${mission?.node} ', style: node),
-          TextSpan(
-            text: '- ${mission?.faction} - ${mission.type}',
-            style: Theme.of(context).textTheme.subhead,
+    // TODO(Ornstein): Need to get a veil proxima skybox but for now the derelict will do
+    return SkyboxCard(
+      node: mission.node,
+      child: Stack(
+        alignment: AlignmentDirectional.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Icon(
+              FactionIcons.sentient,
+              size: 150,
+              color: Colors.grey[600],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('${mission.node}', style: node),
+              const SizedBox(height: 4.0),
+              Text(
+                '${mission.faction} | ${mission.type}',
+                style: Typography.whiteMountainView.subhead,
+              )
+            ],
           )
-        ]),
+        ],
       ),
     );
   }
