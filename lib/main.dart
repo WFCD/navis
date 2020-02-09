563c7b49c6d641cd18c892395f8cb320f86bfc7c
SlayerOrnstein <blacklightinglr@gmail.com>
2020-02-09 00:20:45 -0500

    enabled crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
@@ -10,14 +13,18 @@ import 'injection_container.dart' as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  BlocSupervisor.delegate = await HydratedBlocDelegate.build();
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
-  await di.init();
-  runApp(MultiBlocProvider(
-    providers: [
-      BlocProvider(create: (_) => sl<NavigationBloc>()),
-      BlocProvider(create: (_) => sl<SolsystemBloc>())
-    ],
-    child: const NavisApp(),
-  ));
+  await runZoned(() async {
+    BlocSupervisor.delegate = await HydratedBlocDelegate.build();
+
+    await di.init();
+    runApp(MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => sl<NavigationBloc>()),
+        BlocProvider(create: (_) => sl<SolsystemBloc>())
+      ],
+      child: const NavisApp(),
+    ));
+  }, onError: Crashlytics.instance.recordError);
 }
