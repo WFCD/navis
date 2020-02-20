217eeaa3f5d709607905f7e6a2d048d0d7b48316
SlayerOrnstein <blacklightinglr@gmail.com>
2020-02-19 21:37:41 -0500

    add sortie card

--- lib/features/worldstate/presentation/widgets/feed/feed_cards.dart
+++ lib/features/worldstate/presentation/widgets/feed/feed_cards.dart
@@ -1,4 +1,8 @@
 export 'acolyte_card.dart';
+export 'alerts_card.dart';
+export 'arbitration_card.dart';
 export 'cycle_card.dart';
 export 'event_card.dart';
+export 'kuva_card.dart';
+export 'sortie_card.dart';
 export 'trader_card.dart';
---
+++ lib/features/worldstate/presentation/widgets/feed/sortie_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:navis/core/widgets/widgets.dart';
+import 'package:worldstate_api_model/worldstate_models.dart';
+
+class SortieCard extends StatelessWidget {
+  const SortieCard({Key key, @required this.sortie})
+      : assert(sortie != null),
+        super(key: key);
+
+  final Sortie sortie;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    final boss = textTheme.title;
+    final nodeMission = textTheme.subhead.copyWith(fontSize: 15);
+    final modifier = textTheme.caption.copyWith(fontSize: 13);
+
+    return CustomCard(
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          textBaseline: TextBaseline.alphabetic,
+          children: <Widget>[
+            ListTile(
+              leading: FactionIcon(
+                faction: sortie?.faction ?? 'Grineer',
+                iconSize: 35.0,
+              ),
+              title: Text(sortie?.boss ?? 'Clem', style: boss),
+              trailing: CountdownTimer(
+                expiry: sortie?.expiry ??
+                    DateTime.now().subtract(const Duration(seconds: 60)),
+              ),
+            ),
+            for (final variant in sortie.variants)
+              ListTile(
+                title: Text('${variant.missionType} - ${variant.node}',
+                    style: nodeMission),
+                subtitle: Text(variant.modifier, style: modifier),
+                // dense: true,
+              )
+          ]),
+    );
+  }
+}
