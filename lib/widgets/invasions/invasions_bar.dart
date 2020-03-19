95d9ae73f11b6ea21a9576a814793f7d578915cf
SlayerOrnstein <blacklightinglr@gmail.com>
2020-03-18 23:50:30 -0400

    use a LinearGradient instead of a CustomPainter for invasion progress

--- lib/widgets/invasions/invasions_bar.dart
+++ lib/widgets/invasions/invasions_bar.dart
@@ -6,12 +6,8 @@ class InvasionBar extends StatelessWidget {
   const InvasionBar({
     Key key,
     @required this.progress,
-    this.width,
-    this.padding,
     @required this.attackingFaction,
     @required this.defendingFaction,
-    this.color,
-    this.lineHeight,
   })  : assert(attackingFaction != null),
         assert(defendingFaction != null),
         assert(progress != null),
@@ -19,32 +15,27 @@ class InvasionBar extends StatelessWidget {
 
   final String attackingFaction, defendingFaction;
   final double progress;
-  final double width;
-  final double lineHeight;
-  final EdgeInsets padding;
-  final Color color;
 
   @override
   Widget build(BuildContext context) {
     return Material(
       elevation: 4.0,
       color: Colors.transparent,
-      clipBehavior: Clip.hardEdge,
-      borderRadius: BorderRadius.circular(16.0),
       child: Row(children: <Widget>[
         Expanded(
-          child: Container(
-            height: lineHeight * 2,
-            width: width,
-            padding: padding,
-            child: RepaintBoundary(
-              child: CustomPaint(
-                painter: _InvasionBar(
-                  progress: progress,
-                  progressColor: factionColor(attackingFaction),
-                  backgroundColor: factionColor(defendingFaction),
-                  lineWidth: lineHeight,
-                ),
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 500),
+            height: 20.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4.0),
+              gradient: LinearGradient(
+                colors: <Color>[
+                  factionColor(attackingFaction),
+                  factionColor(defendingFaction)
+                ],
+                stops: <double>[progress, progress / 100],
+                begin: AlignmentDirectional.centerStart,
+                end: AlignmentDirectional.centerEnd,
               ),
             ),
           ),
@@ -52,48 +43,4 @@ class InvasionBar extends StatelessWidget {
       ]),
     );
   }
-}
-
-class _InvasionBar extends CustomPainter {
-  _InvasionBar({
-    this.progress,
-    this.progressColor,
-    this.backgroundColor,
-    this.lineWidth,
-  }) {
-    _attackingFaction.color = progressColor;
-    _attackingFaction.style = PaintingStyle.stroke;
-    _attackingFaction.strokeWidth = lineWidth;
-    _attackingFaction.strokeCap = StrokeCap.butt;
-
-    _defendingFaction.color = backgroundColor;
-    _defendingFaction.style = PaintingStyle.stroke;
-    _defendingFaction.strokeWidth = lineWidth;
-    _defendingFaction.strokeCap = StrokeCap.butt;
-  }
-
-  final Paint _attackingFaction = Paint();
-  final Paint _defendingFaction = Paint();
-  final Color backgroundColor;
-  final Color progressColor;
-  final double progress;
-  final double lineWidth;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final Offset start = Offset(0.0, size.height / 2);
-    final Offset end = Offset(size.width, size.height / 2);
-
-    canvas.drawLine(start, end, _defendingFaction);
-    canvas.drawLine(start, Offset(size.width * progress, size.height / 2),
-        _attackingFaction);
-  }
-
-  @override
-  bool shouldRebuildSemantics(_InvasionBar oldDelegate) =>
-      oldDelegate.progress != progress;
-
-  @override
-  bool shouldRepaint(_InvasionBar oldDelegate) =>
-      oldDelegate.progress != progress;
 }
--- lib/widgets/invasions/invasions_widget.dart
+++ lib/widgets/invasions/invasions_widget.dart
@@ -6,83 +6,42 @@ import 'invasion_details.dart';
 import 'invasion_reward.dart';
 import 'invasions_bar.dart';
 
-class InvasionWidget extends StatefulWidget {
+class InvasionWidget extends StatelessWidget {
   const InvasionWidget({Key key, this.invasion}) : super(key: key);
 
   final Invasion invasion;
 
   @override
-  _InvasionWidgetState createState() => _InvasionWidgetState();
-}
-
-class _InvasionWidgetState extends State<InvasionWidget>
-    with TickerProviderStateMixin, AutomaticKeepAliveClientMixin {
-  Animation<double> _progress;
-  AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-        duration: const Duration(milliseconds: 300), vsync: this);
-
-    _progress =
-        Tween<double>(begin: 0, end: widget.invasion.completion.toDouble())
-            .chain(CurveTween(curve: Curves.easeInOut))
-            .animate(_controller);
-
-    _controller.forward();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    super.build(context);
-
     return SkyboxCard(
       height: 200,
-      node: widget.invasion.node,
+      node: invasion.node,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 24.0),
         child: Column(children: <Widget>[
           const Spacer(),
           InvasionDetails(
-            node: widget.invasion.node,
-            description: widget.invasion.desc,
-            eta: widget.invasion.eta,
+            node: invasion.node,
+            description: invasion.desc,
+            eta: invasion.eta,
           ),
           const Spacer(),
           InvasionReward(
-            attackerReward: widget.invasion.attackerReward.itemString,
-            defenderReward: widget.invasion.defenderReward.itemString,
-            attackingFaction: widget.invasion.attackingFaction,
-            defendingFaction: widget.invasion.defendingFaction,
+            attackerReward: invasion.attackerReward.itemString,
+            defenderReward: invasion.defenderReward.itemString,
+            attackingFaction: invasion.attackingFaction,
+            defendingFaction: invasion.defendingFaction,
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 8.0),
-            child: AnimatedBuilder(
-              animation: _progress,
-              builder: (BuildContext context, Widget child) {
-                return InvasionBar(
-                  attackingFaction: widget.invasion.attackingFaction,
-                  defendingFaction: widget.invasion.defendingFaction,
-                  progress: _progress.value / 100,
-                  lineHeight: 15.0,
-                );
-              },
+            child: InvasionBar(
+              attackingFaction: invasion.attackingFaction,
+              defendingFaction: invasion.defendingFaction,
+              progress: invasion.completion.toDouble() / 100,
             ),
           ),
         ]),
       ),
     );
-  }
-
-  @override
-  bool get wantKeepAlive => true;
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
   }
 }
