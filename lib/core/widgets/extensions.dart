90a81ded85df1bc3331be3458734639d64d333be
SlayerOrnstein <blacklightinglr@gmail.com>
2020-04-22 11:17:45 -0400

    remove duplict file

--- lib/core/widgets/extensions.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-extension DateTimeX on DateTime {
-  String format(String locale) {
-    final dateFormat = DateFormat.yMMMMd(locale).add_jms();
-    return dateFormat.format(this);
-  }
-}
