9182f54424e4e793bb737bb1cde41def99b15f72
SlayerOrnstein <blacklightinglr@gmail.com>
2020-04-22 00:46:55 -0400

    Events: set subtitle to null if there is no tooltip

--- lib/features/worldstate/presentation/widgets/feed/event_card.dart
+++ lib/features/worldstate/presentation/widgets/feed/event_card.dart
@@ -16,7 +16,7 @@ class EventCard extends StatelessWidget {
         for (final event in events)
           ListTile(
             title: Text(event.description),
-            subtitle: Text(event.tooltip ?? ''),
+            subtitle: event.tooltip != null ? Text(event.tooltip) : null,
             trailing: FlatButton(
               onPressed: () {
                 Navigator.of(context)
