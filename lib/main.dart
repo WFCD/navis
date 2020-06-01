e679744d431fc51380ad2c2ecb8f668265401156
SlayerOrnstein <blacklightinglr@gmail.com>
2020-05-31 22:43:59 -0400

    removed lumberdash

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
 
-import 'package:colorize_lumberdash/colorize_lumberdash.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:lumberdash/lumberdash.dart';
 import 'package:navis/injection_container.dart';
+import 'package:logging/logging.dart';
 
 import 'core/app.dart';
 import 'core/bloc/navigation_bloc.dart';
@@ -14,7 +13,9 @@ import 'features/worldstate/presentation/bloc/solsystem_bloc.dart';
 import 'injection_container.dart' as di;
 
 Future<void> main() async {
-  putLumberdashToWork(withClients: [ColorizeLumberdash()]);
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.time}: ${record.message}');
+  });
 
   WidgetsFlutterBinding.ensureInitialized();
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
