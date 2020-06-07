08be0b5bd6b24a4d844afb82ded089a1c02d7b8e
SlayerOrnstein <blacklightinglr@gmail.com>
2020-06-07 14:18:15 -0400

    chore(deploy): use semantic-release to set tag, exluded test from running on tags

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CURRENT_VERSION="$(pubver get |cut -d'+' -f1)"
+
+if [ "$CURRENT_VERSION" != "$1" ]
+then
+    let VERSION_CODE="$(pubver get | cut -d'+' -f2)" 
+    let VERSION_CODE=++VERSION_CODE
+
+    pubver set "$1+$VERSION_CODE"
+fi
