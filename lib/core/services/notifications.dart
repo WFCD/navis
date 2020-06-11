70c7362be4d956332e6941bf3546469c288427c3
SlayerOrnstein <blacklightinglr@gmail.com>
2020-06-11 13:59:58 -0400

    chore: support notifications

---
+++ lib/core/services/notifications.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:logging/logging.dart';
+import 'package:navis/core/network/warframestat_remote.dart';
+
+class NotificationService {
+  factory NotificationService() {
+    final FirebaseMessaging messaging = FirebaseMessaging();
+
+    messaging.requestNotificationPermissions(const IosNotificationSettings(
+        sound: true, badge: true, alert: true, provisional: true));
+
+    messaging.configure();
+
+    return NotificationService._(messaging);
+  }
+
+  NotificationService._(this.messaging);
+
+  final FirebaseMessaging messaging;
+
+  static final _logger = Logger('NotificationService');
+
+  Future<void> subscribeToPlatform(GamePlatforms platform) async {
+    _logger.info('subscribing to ${platform.asString} topic');
+    await messaging.unsubscribeFromTopic(platform.asString);
+  }
+
+  Future<void> unsubscribeFromPlatform(GamePlatforms platform) async {
+    _logger.info('unsubscribing from ${platform.asString} topic');
+    await messaging.subscribeToTopic(platform.asString);
+  }
+
+  Future<void> subscribeToNotification(String topic) async {
+    _logger.info('subscribing to $topic');
+    await messaging.subscribeToTopic(topic);
+  }
+
+  Future<void> unsubscribeFromNotification(String topic) async {
+    _logger.info('unsubscribing from $topic');
+    await messaging.unsubscribeFromTopic(topic);
+  }
+}
