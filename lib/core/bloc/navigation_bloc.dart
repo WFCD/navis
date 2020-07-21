28ec17362f42e3ea2982f8dc324e678752d42e14
SlayerOrnstein <blacklightinglr@gmail.com>
2020-07-21 09:07:56 -0400

    chore: remove hydratedBloc navigationBloc

--- lib/core/bloc/navigation_bloc.dart
+++ lib/core/bloc/navigation_bloc.dart
@@ -2,20 +2,21 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:navis/features/worldstate/presentation/pages/orbiter_news.dart';
 import 'package:rxdart/rxdart.dart';
 
 import '../../features/codex/presentation/pages/codex_search_screen.dart';
 import '../../features/synthtargets/presentation/pages/targets.dart';
 import '../../features/worldstate/presentation/pages/home_feed.dart';
 
-enum NavigationEvent { timers, codex, synthTargets }
+enum NavigationEvent { timers, warframe_news, codex, synthTargets }
 
-class NavigationBloc extends HydratedBloc<NavigationEvent, Widget> {
+class NavigationBloc extends Bloc<NavigationEvent, Widget> {
   NavigationBloc() : super(navigationMap[NavigationEvent.timers]);
 
   static final Map<NavigationEvent, Widget> navigationMap = {
     NavigationEvent.timers: const HomeFeedPage(),
+    NavigationEvent.warframe_news: const OrbiterNewsPage(),
     NavigationEvent.codex: const CodexSearchScreen(),
     NavigationEvent.synthTargets: const SynthTargetsPage()
   };
@@ -35,18 +36,18 @@ class NavigationBloc extends HydratedBloc<NavigationEvent, Widget> {
     yield navigationMap[event];
   }
 
-  @override
-  Widget fromJson(Map<String, dynamic> json) {
-    final event =
-        NavigationEvent.values.firstWhere((n) => n.toString() == json['key']);
+  // @override
+  // Widget fromJson(Map<String, dynamic> json) {
+  //   final event =
+  //       NavigationEvent.values.firstWhere((n) => n.toString() == json['key']);
 
-    return navigationMap[event];
-  }
+  //   return navigationMap[event];
+  // }
 
-  @override
-  Map<String, dynamic> toJson(Widget state) {
-    final key = navigationMap.keys.firstWhere((k) => navigationMap[k] == state);
+  // @override
+  // Map<String, dynamic> toJson(Widget state) {
+  //   final key = navigationMap.keys.firstWhere((k) => navigationMap[k] == state);
 
-    return <String, dynamic>{'key': key.toString()};
-  }
+  //   return <String, dynamic>{'key': key.toString()};
+  // }
 }
