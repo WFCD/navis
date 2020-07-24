9dc744298b15a1d6e0f93db8d63de3557f3ebaeb
SlayerOrnstein <blacklightinglr@gmail.com>
2020-07-24 11:13:15 -0400

    chore: use extension on context for locale

--- lib/core/utils/extensions.dart
+++ lib/core/utils/extensions.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:navis/generated/l10n.dart';
 
 extension DateTimeX on DateTime {
   String format(BuildContext context) {
     return MaterialLocalizations.of(context).formatFullDate(this);
   }
+}
+
+extension ContextX on BuildContext {
+  NavisLocalizations get locale => NavisLocalizations.of(this);
 }
