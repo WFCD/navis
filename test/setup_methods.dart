3f107fa60ade3d13c22ad6df3fc2e7fc25a570cb
SlayerOrnstein <blacklightinglr@gmail.com>
2020-09-21 12:01:18 -0400

    chore(deps):  update dependencies

--- test/setup_methods.dart
+++ test/setup_methods.dart
@@ -4,7 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hive/hive.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mockito/mockito.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
@@ -14,8 +13,6 @@ Future<void> mockSetup() async {
   final directory = await Directory.systemTemp.createTemp();
 
   PathProviderPlatform.instance = MockPathProviderPlatform();
-
-  disablePathProviderPlatformOverride = true;
 
   HydratedBloc.storage = await HydratedStorage.build();
   Hive.init(directory.path);
