d8cb092495fe5136375bf00688fa2b573bdf6594
SlayerOrnstein <blacklightinglr@gmail.com>
2020-10-03 18:35:40 -0400

    chore: init firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -25,6 +26,7 @@ Future<void> main() async {
   });
 
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
   SystemChrome.setSystemUIOverlayStyle(
