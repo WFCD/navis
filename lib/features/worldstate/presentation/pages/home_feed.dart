429bb5609bee75021e748ab8e5b79bcbbcccf5c2
SlayerOrnstein <blacklightinglr@gmail.com>
2020-10-13 22:10:06 -0400

    feat: Display and error message when worldstate has any issues or cached is old

--- lib/features/worldstate/presentation/pages/home_feed.dart
+++ lib/features/worldstate/presentation/pages/home_feed.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:navis/core/widgets/sliver_top_bar.dart';
 import 'package:navis/core/utils/extensions.dart';
+import 'package:navis/features/worldstate/presentation/bloc/solsystem_bloc.dart';
 
 import 'fissures.dart';
 import 'invasions.dart';
@@ -47,35 +49,61 @@ class _HomeFeedPageState extends State<HomeFeedPage>
     }
   }
 
+  void listener(BuildContext context, SolsystemState state) {
+    const displayDuration = Duration(seconds: 50);
+
+    if (state is SystemError) {
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text(state.message),
+        duration: displayDuration,
+      ));
+    } else if (state is SolState) {
+      final now = DateTime.now();
+      final timestamp = state.worldstate.timestamp;
+
+      if (timestamp.difference(now) >= const Duration(minutes: 30)) {
+        Scaffold.of(context).showSnackBar(const SnackBar(
+          content: Text('Worldstate is out of date by more then 30 mins'),
+          duration: displayDuration,
+        ));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: Tabs.values.length,
-      child: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverOverlapAbsorber(
-              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-              sliver: SliverTopbar(
-                pinned: true,
-                child: TabBar(
-                  labelColor:
-                      Theme.of(context).primaryTextTheme.bodyText1.color,
-                  unselectedLabelColor: Theme.of(context)
-                      .primaryTextTheme
-                      .bodyText1
-                      .color
-                      .withOpacity(.7),
-                  indicatorColor: Theme.of(context).accentColor,
-                  tabs: tabs,
+    return BlocListener<SolsystemBloc, SolsystemState>(
+      listener: listener,
+      child: DefaultTabController(
+        length: Tabs.values.length,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverOverlapAbsorber(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                sliver: SliverTopbar(
+                  pinned: true,
+                  child: TabBar(
+                    labelColor:
+                        Theme.of(context).primaryTextTheme.bodyText1.color,
+                    unselectedLabelColor: Theme.of(context)
+                        .primaryTextTheme
+                        .bodyText1
+                        .color
+                        .withOpacity(.7),
+                    indicatorColor: Theme.of(context).accentColor,
+                    tabs: tabs,
+                  ),
                 ),
-              ),
-            )
-          ];
-        },
-        body: const Padding(
+              )
+            ];
+          },
+          body: const Padding(
             padding: EdgeInsets.only(top: kTextTabBarHeight),
-            child: TabBarView(children: _pages)),
+            child: TabBarView(children: _pages),
+          ),
+        ),
       ),
     );
   }
