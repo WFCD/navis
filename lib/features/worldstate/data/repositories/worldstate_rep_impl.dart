22dec950a75cf657f57d302a32156325f4610cc4
SlayerOrnstein <blacklightinglr@gmail.com>
2020-11-04 11:14:08 -0500

    refactor: optimize imports

--- lib/features/worldstate/data/repositories/worldstate_rep_impl.dart
+++ lib/features/worldstate/data/repositories/worldstate_rep_impl.dart
@@ -2,11 +2,11 @@ import 'dart:io';
 
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
-import 'package:navis/core/local/user_settings.dart';
 import 'package:warframestat_api_models/entities.dart';
 import 'package:wfcd_client/wfcd_client.dart';
 
 import '../../../../core/error/failures.dart';
+import '../../../../core/local/user_settings.dart';
 import '../../../../core/network/network_info.dart';
 import '../../domain/repositories/worldstate_repository.dart';
 import '../datasources/warframestate_local.dart';
