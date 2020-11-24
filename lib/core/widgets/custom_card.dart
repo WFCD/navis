79976df0e06a83a86a256f21aef31d67025d88db
SlayerOrnstein <blacklightinglr@gmail.com>
2020-11-24 13:38:47 -0500

    chore: use flutter's card code but with an AnimatedContainer

--- lib/core/widgets/custom_card.dart
+++ lib/core/widgets/custom_card.dart
@@ -48,23 +48,45 @@ class CustomCard extends StatelessWidget {
     );
   }
 
+  static const double _defaultElevation = 1.0;
+  static const _semanticContainer = true;
+
   @override
   Widget build(BuildContext context) {
-    final cardContent = Padding(
-      padding: padding,
-      child: title != null
-          ? Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[_buildTitle(context, title), child],
-            )
-          : child,
-    );
+    final ThemeData theme = Theme.of(context);
+    final CardTheme cardTheme = CardTheme.of(context);
+    final cardContent = title != null
+        ? Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[_buildTitle(context, title), child],
+          )
+        : child;
 
-    return Card(
-      color: color,
-      margin: margin,
-      child: addBanner ? _wrapWithBanner(context, cardContent) : cardContent,
+    return Semantics(
+      container: _semanticContainer,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        margin: margin ?? cardTheme.margin ?? const EdgeInsets.all(4.0),
+        padding: padding,
+        child: Material(
+          type: MaterialType.card,
+          shadowColor: cardTheme.shadowColor ?? theme.shadowColor,
+          color: color ?? cardTheme.color ?? theme.cardColor,
+          elevation: cardTheme.elevation ?? _defaultElevation,
+          shape: cardTheme.shape ??
+              const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(4.0)),
+              ),
+          borderOnForeground: true,
+          clipBehavior: cardTheme.clipBehavior ?? Clip.none,
+          child: Semantics(
+            explicitChildNodes: !_semanticContainer,
+            child:
+                addBanner ? _wrapWithBanner(context, cardContent) : cardContent,
+          ),
+        ),
+      ),
     );
   }
 }
