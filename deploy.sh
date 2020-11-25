a4f724ea811f8ab2597831ac654d45099f2bcbc0
SlayerOrnstein <blacklightinglr@gmail.com>
2020-11-25 00:56:07 -0500

    chore(actions): run cider directly with pub

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CURRENT_VERSION="$(cider version | cut -d'+' -f1)"
+CURRENT_VERSION="$(pub global run cider version | cut -d'+' -f1)"
 
 if [ "$CURRENT_VERSION" != "$1" ]
 then
-    let VERSION_CODE="$(cider version | cut -d'+' -f2)" 
+    let VERSION_CODE="$(pub global run cider version | cut -d'+' -f2)" 
     let VERSION_CODE=++VERSION_CODE
 
     cider version "$1+$VERSION_CODE"
