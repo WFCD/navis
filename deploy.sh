fed8843a53297e070ffd190bb671c0ca032f86b9
SlayerOrnstein <blacklightinglr@gmail.com>
2020-11-25 01:06:31 -0500

    chore(actions): run export in deply script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-CURRENT_VERSION="$(pub global run cider version | cut -d'+' -f1)"
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+
+CURRENT_VERSION="$(cider version | cut -d'+' -f1)"
 
 if [ "$CURRENT_VERSION" != "$1" ]
 then
-    let VERSION_CODE="$(pub global run cider version | cut -d'+' -f2)" 
+    let VERSION_CODE="$(cider version | cut -d'+' -f2)" 
     let VERSION_CODE=++VERSION_CODE
 
     cider version "$1+$VERSION_CODE"
