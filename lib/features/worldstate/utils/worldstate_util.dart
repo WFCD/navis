bca1f7c1133e3d0f05546d40883eac1a171971d9
SlayerOrnstein <blacklightinglr@gmail.com>
2021-01-14 16:37:10 -0500

    fix: better filtering of expired fissures and invasions (#83)

--- lib/features/worldstate/utils/worldstate_util.dart
+++ lib/features/worldstate/utils/worldstate_util.dart
@@ -1,26 +1,23 @@
 import 'package:wfcd_client/entities.dart';
 
 Worldstate cleanState(Worldstate state) {
-  state.alerts.removeWhere((a) {
-    return a.active == false ||
-        a.expiry.difference(DateTime.now().toUtc()) <=
-            const Duration(seconds: 1);
-  });
+  state.alerts.retainWhere((e) => e.active);
 
-  state.dailyDeals.retainWhere((d) {
-    return d.total - d.sold > 0 ||
-        d.expiry.difference(DateTime.now()) > const Duration(seconds: 60);
-  });
+  state.dailyDeals.retainWhere((e) => (e.total - e.sold) > 0);
 
   state.news.sort((a, b) => b.date.compareTo(a.date));
 
   state.persistentEnemies.sort((a, b) => a.agentType.compareTo(b.agentType));
 
-  state.syndicateMissions.retainWhere((s) => s.jobs?.isNotEmpty ?? false);
+  state.syndicateMissions
+    ..retainWhere((s) => s.jobs?.isNotEmpty ?? false)
+    ..sort((a, b) => a.name.compareTo(b.name));
 
-  state.syndicateMissions.sort((a, b) => a.name.compareTo(b.name));
+  state.fissures
+    ..retainWhere((e) => !e.expired)
+    ..sort((a, b) => a.tierNum.compareTo(b.tierNum));
 
-  state.fissures.sort((a, b) => a.tierNum.compareTo(b.tierNum));
+  state.invasions.retainWhere((e) => !e.completed);
 
   return state;
 }
