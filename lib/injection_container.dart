d0a40fe2a2f56ddd7c2e05aa2870bb9faf499a60
SlayerOrnstein <blacklightinglr@gmail.com>
2021-02-24 03:01:35 -0500

    chore: remove local notification package
    giving it some thought it would be better if it gets implmented sever
    side.

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,5 @@
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get_it/get_it.dart';
 import 'package:package_info/package_info.dart';
 import 'package:wfcd_client/wfcd_client.dart';
@@ -30,20 +29,8 @@ import 'features/worldstate/presentation/bloc/solsystem_bloc.dart';
 final GetIt sl = GetIt.instance;
 
 Future<void> init() async {
-  const initializationSettingsAndroid =
-      AndroidInitializationSettings('@drawable/ic_nightmare');
-  const initializationSettingsIOS = IOSInitializationSettings();
-  const initializationSettings = InitializationSettings(
-      android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
-
-  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-
   // Core
   sl
-    ..registerSingleton<FlutterLocalNotificationsPlugin>(
-        flutterLocalNotificationsPlugin)
     ..registerSingleton<NetworkInfo>(NetworkInfoImpl(DataConnectionChecker()))
     ..registerSingletonAsync<EventInfoParser>(EventInfoParser.loadEventData)
     ..registerSingleton<VideoService>(VideoService())
