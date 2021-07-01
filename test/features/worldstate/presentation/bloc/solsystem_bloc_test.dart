f3d4fa0a7955c818ae3386bd0a16514c4da818d9
SlayerOrnstein <HydroNav@protonmail.com>
2021-07-01 19:35:32 -0400

    test: check that the initial state is SolsystemInitial
    test: make sure to double sync state with different forceUpdate values

--- test/features/worldstate/presentation/bloc/solsystem_bloc_test.dart
+++ test/features/worldstate/presentation/bloc/solsystem_bloc_test.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:navis/features/worldstate/data/repositories/worldstate_rep_impl.dart';
-import 'package:navis/features/worldstate/domain/usecases/get_darvo_deal_info.dart';
 import 'package:navis/features/worldstate/domain/usecases/get_worldstate.dart';
 import 'package:navis/features/worldstate/presentation/bloc/solsystem_bloc.dart';
 import 'package:oxidized/oxidized.dart';
@@ -13,25 +11,14 @@ import 'package:wfcd_client/wfcd_client.dart';
 
 import '../../../../fixtures/fixture_reader.dart';
 
-// ignore: must_be_immutable
 class MockGetWorldstate extends Mock implements GetWorldstate {}
-
-// ignore: must_be_immutable
-class MockGetDarvoDealInfo extends Mock implements GetDarvoDealInfo {}
-
-class DealRequestFake extends Mock implements DealRequest {}
 
 void main() {
   final worldstateFixture = fixture('worldstate.json');
-  final resultsFixture = fixture('darvo_deal_test.json');
-
   final tWorldstate =
       toWorldstate(json.decode(worldstateFixture) as Map<String, dynamic>);
 
-  final tResults = toBaseItems(json.decode(resultsFixture) as List<dynamic>);
-
   late GetWorldstate getWorldstate;
-  late GetDarvoDealInfo getDarvoDealInfo;
 
   late SolsystemBloc solsystemBloc;
 
@@ -41,40 +28,30 @@ void main() {
     );
 
     getWorldstate = MockGetWorldstate();
-    getDarvoDealInfo = MockGetDarvoDealInfo();
 
     solsystemBloc = SolsystemBloc(getWorldstate: getWorldstate);
 
-    registerFallbackValue<DealRequest>(DealRequestFake());
-
     when(() => getWorldstate(any())).thenAnswer((_) async => Ok(tWorldstate));
-    when(() => getDarvoDealInfo(any()))
-        .thenAnswer((_) async => Ok(tResults.first));
   });
 
-  tearDown(() {
+  tearDownAll(() {
     solsystemBloc.close();
+    HydratedBloc.storage.clear();
   });
 
-  // test('initialState should be [SolsystemInitial]', () async {
-  //   expect(solsystemBloc.state, equals(SolsystemInitial()));
-  // });
+  test('initialState should be [SolsystemInitial]', () {
+    expect(solsystemBloc.state, equals(SolsystemInitial()));
+  });
 
   test('should sync solsystem status', () async {
-    solsystemBloc.add(const SyncSystemStatus());
+    await solsystemBloc.update();
     await untilCalled(() => getWorldstate(any()));
 
     verify(() => getWorldstate(false));
+
+    await solsystemBloc.update(forceUpdate: true);
+    await untilCalled(() => getWorldstate(any()));
+
+    verify(() => getWorldstate(true));
   });
-
-  // group('retrive darvo deal information', () {
-  //   test('should search a BaseItem instance based on the current daily deal',
-  //       () async {
-  //     solsystemBloc.add(const SyncSystemStatus(GamePlatforms.pc));
-  //     await untilCalled(getDarvoDealInfo(any));
-
-  //     await Future<void>.delayed(300.milliseconds);
-  //     verify(getDarvoDealInfo(any));
-  //   });
-  // });
 }
