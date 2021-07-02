6a579f308787fecc23bdfe62bbf13428c224f5e0
SlayerOrnstein <HydroNav@protonmail.com>
2021-07-01 20:02:57 -0400

    test: let video url come up as null so it doesn't throw when it actually is

--- test/core/services/videos._test.dart
+++ test/core/services/videos._test.dart
@@ -11,6 +11,6 @@ void main() {
   test('Test video retrivial', () async {
     final video = await videoService.getVideoInformation('dQw4w9WgXcQ');
 
-    expect(video!.url, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    expect(video?.url, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
   });
 }
