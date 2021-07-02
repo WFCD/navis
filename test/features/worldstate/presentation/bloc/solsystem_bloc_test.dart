5706723e570a4a02c6cbc33267cac070a6158395
SlayerOrnstein <HydroNav@protonmail.com>
2021-07-01 23:06:41 -0400

    test: add a few more test for Solsystembloc

--- test/features/worldstate/presentation/bloc/solsystem_bloc_test.dart
+++ test/features/worldstate/presentation/bloc/solsystem_bloc_test.dart
@@ -3,10 +3,12 @@ import 'dart:io';
 
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:navis/core/error/exceptions.dart';
 import 'package:navis/features/worldstate/domain/usecases/get_worldstate.dart';
 import 'package:navis/features/worldstate/presentation/bloc/solsystem_bloc.dart';
 import 'package:oxidized/oxidized.dart';
 import 'package:test/test.dart';
+import 'package:wfcd_client/models.dart';
 import 'package:wfcd_client/wfcd_client.dart';
 
 import '../../../../fixtures/fixture_reader.dart';
@@ -28,10 +30,7 @@ void main() {
     );
 
     getWorldstate = MockGetWorldstate();
-
     solsystemBloc = SolsystemBloc(getWorldstate: getWorldstate);
-
-    when(() => getWorldstate(any())).thenAnswer((_) async => Ok(tWorldstate));
   });
 
   tearDownAll(() {
@@ -39,19 +38,52 @@ void main() {
     HydratedBloc.storage.clear();
   });
 
-  test('initialState should be [SolsystemInitial]', () {
+  test('InitialState should be SolsystemInitial', () {
     expect(solsystemBloc.state, equals(SolsystemInitial()));
   });
 
   test('should sync solsystem status', () async {
+    when(() => getWorldstate(any())).thenAnswer((_) async => Ok(tWorldstate));
+
     await solsystemBloc.update();
     await untilCalled(() => getWorldstate(any()));
 
     verify(() => getWorldstate(false));
-
-    await solsystemBloc.update(forceUpdate: true);
-    await untilCalled(() => getWorldstate(any()));
-
-    verify(() => getWorldstate(true));
+    expect(solsystemBloc.state, SolState(tWorldstate));
   });
+
+  group('Test bloc exceptions', () {
+    test('Server exception is throw', () async {
+      when(() => getWorldstate(any())).thenThrow(ServerException());
+
+      await solsystemBloc.update();
+      await untilCalled(() => getWorldstate(any()));
+
+      verify(() => getWorldstate(false));
+      expect(solsystemBloc.state, const SystemError(serverFailureMessage));
+    });
+
+    test('Cache exception is thrown', () async {
+      when(() => getWorldstate(any())).thenThrow(CacheException());
+
+      await solsystemBloc.update();
+      await untilCalled(() => getWorldstate(any()));
+
+      verify(() => getWorldstate(false));
+      expect(solsystemBloc.state, const SystemError(cacheFailureMessage));
+    });
+  });
+
+  test(
+    'Test to make sure the worldstate was saved into the Hydrated hive box',
+    () async {
+      when(() => getWorldstate(any())).thenAnswer((_) async => Ok(tWorldstate));
+      await solsystemBloc.update();
+
+      final cached = HydratedBloc.storage.read(solsystemBloc.storageToken);
+      final worldstate = WorldstateModel.fromJson(cached);
+
+      expect(worldstate, tWorldstate);
+    },
+  );
 }
