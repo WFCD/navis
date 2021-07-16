87933bc2e899669deaf636362ea0738a28c9802a
SlayerOrnstein <HydroNav@protonmail.com>
2021-07-15 22:48:19 -0400

    chore: more flexible way of rendering entry info

--- lib/features/codex/presentation/widgets/codex_entry/entry_info.dart
+++ lib/features/codex/presentation/widgets/codex_entry/entry_info.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
@@ -35,75 +37,37 @@ class BasicItemInfo extends SliverPersistentHeaderDelegate {
     return Container(
       height: expandedHeight,
       color: Theme.of(context).canvasColor,
-      child: SingleChildScrollView(
-        physics: const NeverScrollableScrollPhysics(),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            AppBar(
-              elevation: 0.0,
-              backgroundColor: Theme.of(context).canvasColor,
-              iconTheme: Theme.of(context).iconTheme,
-              brightness: Theme.of(context).brightness,
-              actions: [
-                TextButton(
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all(
-                        Theme.of(context).textTheme.button?.color),
-                  ),
-                  onPressed: () => wikiaUrl?.launchLink(context),
-                  child: Text(context.l10n.seeWikia),
+      child: Stack(
+        children: <Widget>[
+          AppBar(
+            elevation: 0.0,
+            backgroundColor: Theme.of(context).canvasColor,
+            iconTheme: Theme.of(context).iconTheme,
+            brightness: Theme.of(context).brightness,
+            actions: [
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(
+                      Theme.of(context).textTheme.button?.color),
                 ),
-              ],
-            ),
-            AnimatedOpacity(
-              duration: kThemeAnimationDuration,
-              opacity: 1 - (shrinkOffset / expandedHeight),
-              child: AnimatedContainer(
-                duration: kThemeAnimationDuration,
-                width: MediaQuery.of(context).size.width,
-                height: shrinkOffset > 0.0 ? 0 : (expandedHeight / 100) * 50,
-                child: FittedBox(
-                  fit: BoxFit.fitHeight,
-                  clipBehavior: Clip.hardEdge,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Hero(
-                          tag: uniqueName,
-                          child: CircleAvatar(
-                            backgroundImage:
-                                CachedNetworkImageProvider(imageUrl),
-                            backgroundColor: Colors.grey,
-                            radius: (MediaQuery.of(context).size.longestSide /
-                                    100) *
-                                6,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        name,
-                        style: textTheme.subtitle1,
-                        textAlign: textAlign,
-                      ),
-                      SizedBox(
-                        width: (MediaQuery.of(context).size.width / 100) * 95,
-                        child: Text(
-                          description,
-                          style: textTheme.caption,
-                          textAlign: textAlign,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+                onPressed: () => wikiaUrl?.launchLink(context),
+                child: Text(context.l10n.seeWikia),
               ),
+            ],
+          ),
+          Center(
+            child: _EntryInfoContent(
+              height: expandedHeight,
+              shrinkOffset: shrinkOffset,
+              uniqueName: uniqueName,
+              imageUrl: imageUrl,
+              name: name,
+              description: description,
             ),
-            if (bottom != null) bottom!
-          ],
-        ),
+          ),
+          if (bottom != null)
+            Align(alignment: Alignment.bottomCenter, child: bottom!)
+        ],
       ),
     );
   }
@@ -117,5 +81,73 @@ class BasicItemInfo extends SliverPersistentHeaderDelegate {
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
     return false;
+  }
+}
+
+class _EntryInfoContent extends StatelessWidget {
+  const _EntryInfoContent({
+    Key? key,
+    required this.height,
+    required this.shrinkOffset,
+    required this.uniqueName,
+    required this.imageUrl,
+    required this.name,
+    required this.description,
+  }) : super(key: key);
+
+  final double shrinkOffset, height;
+  final String uniqueName;
+  final String imageUrl;
+  final String name;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    const textAlign = TextAlign.center;
+    final textTheme = Theme.of(context).textTheme;
+
+    return AnimatedOpacity(
+      duration: kThemeAnimationDuration,
+      opacity: 1 - (shrinkOffset / height),
+      child: AnimatedContainer(
+        duration: kThemeAnimationDuration,
+        width: MediaQuery.of(context).size.width,
+        height: shrinkOffset > 0.0 ? 0 : 106,
+        child: FittedBox(
+          clipBehavior: Clip.hardEdge,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 6.0),
+                child: Hero(
+                  tag: uniqueName,
+                  child: CircleAvatar(
+                    backgroundImage: CachedNetworkImageProvider(imageUrl),
+                    backgroundColor: Colors.grey,
+                    radius:
+                        (MediaQuery.of(context).size.shortestSide / 100) * 8,
+                  ),
+                ),
+              ),
+              Text(
+                name,
+                style: textTheme.subtitle1,
+                textAlign: textAlign,
+              ),
+              SizedBox(
+                width: (MediaQuery.of(context).size.width / 100) * 95,
+                child: Text(
+                  description,
+                  style: textTheme.caption,
+                  textAlign: textAlign,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
