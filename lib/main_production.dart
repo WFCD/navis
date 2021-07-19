98a4e51d1f0406891791b59018e76782e68ceb91
SlayerOrnstein <HydroNav@protonmail.com>
2021-07-19 16:58:02 -0400

    fix: add extra context for sentry reports

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,11 +1,24 @@
 import 'package:matomo/matomo.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
+import 'core/local/user_settings.dart';
+import 'injection_container.dart';
 import 'start_app.dart';
 
 Future<void> main() async {
   await SentryFlutter.init(
-    (option) => option.dsn = const String.fromEnvironment('SENTRY_DSN'),
+    (option) {
+      option
+        ..dsn = const String.fromEnvironment('SENTRY_DSN')
+        ..beforeSend = (SentryEvent event, {dynamic hint}) {
+          final usersettings = sl<Usersettings>();
+
+          return event.copyWith(extra: {
+            'gamePlatform': usersettings.platform,
+            'appLanguage': usersettings.language
+          });
+        };
+    },
     appRunner: () async {
       await MatomoTracker().initialize(
           siteId: 2, url: const String.fromEnvironment('MATOMO_URL'));
