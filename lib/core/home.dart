53e2569ae82165911382e418ac37c4210d2b72e6
SlayerOrnstein <HydroNav@protonmail.com>
2021-09-21 21:53:09 -0400

    refactor: use the proper duration for page switching

--- lib/core/home.dart
+++ lib/core/home.dart
@@ -53,7 +53,7 @@ class _HomeState extends State<Home> {
         body: BlocBuilder<NavigationCubit, Widget>(
           builder: (BuildContext context, Widget state) {
             return AnimatedSwitcher(
-              duration: kDelayShort,
+              duration: kAnimationShort,
               child: state,
             );
           },
