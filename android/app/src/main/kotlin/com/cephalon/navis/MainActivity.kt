67a9e999b0ce85f6832de569f6540842da592a7e
SlayerOrnstein <HydroNav@protonmail.com>
2021-12-10 07:36:31 -0500

    feat: migrated to Android S splashscreen

--- android/app/src/main/kotlin/com/cephalon/navis/MainActivity.kt
+++ android/app/src/main/kotlin/com/cephalon/navis/MainActivity.kt
@@ -1,5 +1,19 @@
 package com.cephalon.navis
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+        }
+
+        super.onCreate(savedInstanceState)
+    }
+}
