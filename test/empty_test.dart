1b32510e487e33302ce69b944b00a9fc79cf6154
SlayerOrnstein <HydroNav@protonmail.com>
2021-12-11 02:04:21 -0500

    test: add an empty test
    rewriting test so in the mean time if it works it works

---
+++ test/empty_test.dart
@@ -0,0 +1,8 @@
+// Just making an empty test while I rewrite the test to be more in-deph
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('empty test', () {
+    equals(true).matches(true, <dynamic, dynamic>{});
+  });
+}
