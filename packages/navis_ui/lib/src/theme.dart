c3bf201d9032372f9fec6c4c68a468e3441a64b9
SlayerOrnstein <HydroNav@protonmail.com>
2022-02-15 17:38:54 -0500

    Revert "chore(theme): don't override appbar color"
    This reverts commit cb61dd6c5c3a278bf79f2d562c4797030bf960b7.

--- packages/navis_ui/lib/src/theme.dart
+++ packages/navis_ui/lib/src/theme.dart
@@ -8,7 +8,7 @@ class NavisTheme {
   static ThemeData get standard {
     return ThemeData(
       colorScheme: _colorScheme(),
-      // appBarTheme: _appBarTheme,
+      appBarTheme: _appBarTheme,
       cardTheme: _cardTheme,
       dialogTheme: _dialogTheme,
       navigationBarTheme: _navigationBarThemeData,
@@ -17,6 +17,7 @@ class NavisTheme {
 
   static ThemeData get dark {
     return ThemeData(
+      appBarTheme: _appBarTheme,
       colorScheme: _colorScheme(Brightness.dark),
       cardTheme: _cardTheme.copyWith(color: Colors.grey[900]),
       dialogTheme: const DialogTheme(backgroundColor: NavisColors.canvasColor),
@@ -26,27 +27,15 @@ class NavisTheme {
     );
   }
 
-  // The way I'm unserstanding it using ColorScheme.copyWith() is broken in that
-  // passing Brightness to it doesn't recreate the colors.
-  //
-  // i.e if the ColorScheme was created with Brightness.dark then using
-  // copyWith to change it to Brightness.light will only pass down the new
-  // Brightness value but also the colors that were generated the first time.
-  // Causing the new ColorScheme to have Brightness.light mixed with
-  // Brightness.dark ColorScheme.
-  static ColorScheme _colorScheme([Brightness brightness = Brightness.light]) {
-    return ColorScheme.fromSeed(
-      seedColor: generateMaterialColor(NavisColors.blue),
-      secondary: NavisColors.secondary,
-      brightness: brightness,
-    );
+  static AppBarTheme get _appBarTheme {
+    return const AppBarTheme(color: NavisColors.blue);
   }
 
   static CardTheme get _cardTheme {
     return CardTheme(
       elevation: 6,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-      margin: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
       clipBehavior: Clip.hardEdge,
     );
   }
@@ -59,8 +48,8 @@ class NavisTheme {
 
   static NavigationBarThemeData get _navigationBarThemeData {
     return NavigationBarThemeData(
+      backgroundColor: NavisColors.canvasColor,
       indicatorColor: Colors.transparent,
-      height: 60,
       iconTheme: MaterialStateProperty.resolveWith((states) {
         if (states.contains(MaterialState.selected)) {
           return const IconThemeData(color: NavisColors.secondary);
