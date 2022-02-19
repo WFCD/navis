ff50c2ead7db984e202a1ca32cd6f28c04fe2961
SlayerOrnstein <HydroNav@protonmail.com>
2022-02-18 19:16:36 -0500

    refactor: move bundled assets into it's own folder

--- lib/resources/navis_assets.dart
+++ lib/resources/navis_assets.dart
@@ -3,10 +3,8 @@ part of 'resources.dart';
 class NavisAssets {
   NavisAssets._();
 
-  static const String appStoreBadge = 'assets/app-store-badge.svg';
-  static const String derelict = 'assets/Derelict.webp';
-  static const String eventInfo = 'assets/event_info.json';
-  static const String googlePlayBadge = 'assets/google-play-badge.svg';
-  static const String wfcdBanner = 'assets/wfcd_banner.svg';
-  static const String wfcdLogoColor = 'assets/wfcd_logo_color.svg';
+  static const String derelict = 'assets/bundled_assets/Derelict.webp';
+  static const String eventInfo = 'assets/bundled_assets/event_info.json';
+  static const String wfcdLogoColor =
+      'assets/bundled_assets/wfcd_logo_color.svg';
 }
