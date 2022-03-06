a6f249d1bc83211ca0e2196f3de3fcbe973ecf91
SlayerOrnstein <HydroNav@protonmail.com>
2022-03-06 01:52:28 -0500

    chore:(timer): add a baro banner background and streamlined desgin a bit

--- lib/worldstate/widgets/timers/trader_card.dart
+++ lib/worldstate/widgets/timers/trader_card.dart
@@ -4,71 +4,50 @@ import 'package:navis/l10n/l10n.dart';
 import 'package:navis/worldstate/cubits/solsystem_cubit.dart';
 import 'package:navis/worldstate/views/trader_inventory.dart';
 import 'package:navis_ui/navis_ui.dart';
-import 'package:wfcd_client/entities.dart';
 
 class TraderCard extends StatelessWidget {
   const TraderCard({Key? key}) : super(key: key);
 
-  Widget _buildButton(BuildContext context, List<InventoryItem> inventory) {
-    final width = MediaQuery.of(context).size.width;
-
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(minimumSize: Size(width, 36)),
-      onPressed: () => Navigator.of(context)
-          .pushNamed(BaroInventory.route, arguments: inventory),
-      child: Text(
-        context.l10n.baroInventory,
-        style: const TextStyle(fontSize: 17, color: Colors.white),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    const padding = EdgeInsets.symmetric(horizontal: 4, vertical: 4);
-
     return BlocBuilder<SolsystemCubit, SolsystemState>(
       builder: (context, state) {
         final trader = (state as SolState).worldstate.voidTrader;
         final l10n = context.l10n;
 
-        final date = trader.active ? trader.expiry! : trader.activation!;
+        final date = MaterialLocalizations.of(context).formatFullDate(
+          trader.active ? trader.expiry! : trader.activation!,
+        );
+        final status = trader.active ? l10n.baroLeaving : l10n.baroArriving;
 
-        return AppCard(
-          title: l10n.baroTitle,
-          child: Column(
-            children: <Widget>[
-              RowItem(
-                text:
-                    Text(trader.active ? l10n.baroLeaving : l10n.baroArriving),
-                padding: padding,
-                child: CountdownTimer(
-                  tooltip: l10n.countdownTooltip(date),
-                  expiry: date,
-                ),
-              ),
-              if (trader.active)
-                RowItem(
-                  text: Text(l10n.baroLocation),
-                  padding: padding,
-                  child: ColoredContainer.text(
-                    text: trader.location,
-                    color: Theme.of(context).colorScheme.primary,
+        return InkWell(
+          onTap: trader.active
+              ? () => Navigator.of(context)
+                  .pushNamed(BaroInventory.route, arguments: trader.inventory)
+              : null,
+          child: AppCard(
+            padding: EdgeInsets.zero,
+            child: SizedBox(
+              height: 150,
+              child: ImageContainer(
+                imageProvider: const AssetImage('assets/baro_banner.webp'),
+                height: 150,
+                padding: EdgeInsets.zero,
+                child: ListTile(
+                  title: Text(l10n.baroTitle),
+                  subtitle: Text(
+                    trader.active
+                        ? 'Tap for Inventory\n$status $date'
+                        : '$status $date',
+                  ),
+                  trailing: CountdownTimer(
+                    tooltip: l10n.countdownTooltip(date),
+                    color: const Color(0xFF82598b),
+                    expiry: trader.active ? trader.expiry! : trader.activation!,
                   ),
                 ),
-              RowItem(
-                text: Text(
-                  trader.active ? l10n.baroLeavesOn : l10n.baroArrivesOn,
-                ),
-                padding: padding,
-                child: ColoredContainer.text(
-                  color: Theme.of(context).colorScheme.primary,
-                  text: MaterialLocalizations.of(context).formatFullDate(date),
-                ),
               ),
-              SizedBoxSpacer.spacerHeight2,
-              if (trader.active) _buildButton(context, trader.inventory)
-            ],
+            ),
           ),
         );
       },
