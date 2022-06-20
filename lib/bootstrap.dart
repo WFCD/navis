66417da55d6586fe5fb32c23d44ba8125db05118
SlayerOrnstein <HydroNav@protonmail.com>
2022-06-20 12:01:41 -0400

    refactor: explicit dynamics

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -19,13 +19,13 @@ import 'package:worldstate_repository/worldstate_repository.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
-  void onChange(BlocBase bloc, Change change) {
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
     log('onChange(${bloc.runtimeType}), ${change.toString()}');
   }
 
   @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
     log('onError(${bloc.runtimeType}), $error, $stackTrace');
     super.onError(bloc, error, stackTrace);
   }
