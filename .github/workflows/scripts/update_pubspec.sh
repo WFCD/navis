a8afb7bd25e273c500d6f622ff20c8a5e8b1edbf
SlayerOrnstein <HydroNav@protonmail.com>
2022-09-05 18:21:03 -0400

    ci: script error when bumping pubspec version

--- .github/workflows/scripts/update_pubspec.sh
+++ .github/workflows/scripts/update_pubspec.sh
@@ -3,9 +3,8 @@
 # export PATH="$PATH":"$HOME/.pub-cache/bin"
 
 let CURRENT_VERSION="$(cider version | cut -d'+' -f2)"
-let VERSION_CODE=$((CURRENT_VERSION + 1))
 
-cider version "$NEW_RELEASE+$VERSION_CODE"
+cider version "$NEW_RELEASE+$((CURRENT_VERSION + 1))"
 
 git config --global user.name "wfcd-bot-boi"
 git config --global user.email "wfcd-bot-boi@users.noreply.github.com"
