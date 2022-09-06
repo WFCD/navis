e4f3d69356463c1b6e20e4340e794a673e6694e4
SlayerOrnstein <HydroNav@protonmail.com>
2022-09-05 20:39:06 -0400

    ci(release): revert changes done to SR config

--- .github/workflows/scripts/update_pubspec.sh
+++ .github/workflows/scripts/update_pubspec.sh
@@ -2,15 +2,7 @@
 
 # export PATH="$PATH":"$HOME/.pub-cache/bin"
 
-CURRENT_VERSION="$(cider version | cut -d'+' -f2)"
+let VERSION_CODE="$(cider version | cut -d'+' -f2)" 
+let VERSION_CODE=++VERSION_CODE
 
-cider version "$NEW_RELEASE+$((CURRENT_VERSION + 1))"
-
-git config --global user.name "wfcd-bot-boi"
-git config --global user.email "wfcd-bot-boi@users.noreply.github.com"
-
-git add pubspec.yaml
-git commit -a -m "chore(release): $NEW_RELEASE"
-
-git pull --rebase
-git push
+cider version "$1+$VERSION_CODE"
