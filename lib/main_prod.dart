a7a3c141f4a6b4b8e52fb27f54ebdda608677a3f
SlayerOrnstein <HydroNav@protonmail.com>
2022-09-10 17:18:44 -0400

    chore: add sentry logging intergrations

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -2,10 +2,15 @@ import 'package:matomo/matomo.dart';
 import 'package:navis/app/app.dart';
 import 'package:navis/bootstrap.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:sentry_logging/sentry_logging.dart';
 
 Future<void> main() async {
   await SentryFlutter.init(
-    (option) => option.dsn = const String.fromEnvironment('SENTRY_DSN'),
+    (option) {
+      option
+        ..dsn = const String.fromEnvironment('SENTRY_DSN')
+        ..addIntegration(LoggingIntegration());
+    },
     appRunner: () async {
       await MatomoTracker().initialize(
         siteId: 2,
