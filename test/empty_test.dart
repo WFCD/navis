c171d25642144338ee17cdc1dd028ebb6143e011
SlayerOrnstein <HydroNav@protonmail.com>
2022-09-18 00:29:15 -0400

    refactored that one empty test

--- test/empty_test.dart
+++ test/empty_test.dart
@@ -1,4 +1,4 @@
-// Just making an empty test while I rewrite the test to be more in-deph
+// Just making an empty test while I rewrite the test to be more in-deph.
 import 'package:test/test.dart';
 
 void main() {
