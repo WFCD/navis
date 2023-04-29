7bf9b712ffb3485041c339fcf11c51419a01e657
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2023-04-29 16:24:14 -0400

    fix: droping sentry breadcrumbs in an effort to track down a bug with darvo deals

--- lib/worldstate/cubits/deals/darvodeal_bloc.dart
+++ lib/worldstate/cubits/deals/darvodeal_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:navis/worldstate/cubits/deals/darvodeal_state.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:warframestat_client/warframestat_client.dart';
 import 'package:worldstate_repository/worldstate_repository.dart';
 
@@ -12,6 +13,14 @@ class DarvodealCubit extends HydratedCubit<DarvodealState> {
 
   Future<void> fetchDeal(String id, String name) async {
     emit(DarvodealLoading());
+
+    final crumb = Breadcrumb(
+      type: 'DarvoDeal',
+      category: 'Deal Loading',
+      data: {'id': id, 'name': name},
+    );
+
+    await Sentry.addBreadcrumb(crumb);
 
     try {
       final info = await repository.getDealInfo(id, name);
@@ -29,15 +38,31 @@ class DarvodealCubit extends HydratedCubit<DarvodealState> {
 
   @override
   DarvodealState fromJson(Map<String, dynamic> json) {
-    final items = toItem(json['items'] as Map<String, dynamic>);
+    final item = toItem(json['item'] as Map<String, dynamic>);
 
-    return DarvoDealLoaded(items);
+    final crumb = Breadcrumb(
+      type: 'DarvoDeal',
+      category: 'Bloc Hydrating',
+      data: {'item': item},
+    );
+
+    Sentry.addBreadcrumb(crumb);
+
+    return DarvoDealLoaded(item);
   }
 
   @override
   Map<String, dynamic>? toJson(DarvodealState state) {
     if (state is DarvoDealLoaded) {
-      return <String, dynamic>{'items': state.item.toJson()};
+      final crumb = Breadcrumb(
+        type: 'DarvoDeal',
+        category: 'Bloc Hydrated',
+        data: {'item': state.item.toJson()},
+      );
+
+      Sentry.addBreadcrumb(crumb);
+
+      return <String, dynamic>{'item': state.item.toJson()};
     }
 
     return null;
