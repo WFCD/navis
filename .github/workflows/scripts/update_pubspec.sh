1aeeecdb57c6c8f64a9dd1f4a619059104fdfa9a
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-02-08 15:47:08 -0500

    ci: improve release workflow

--- .github/workflows/scripts/update_pubspec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH="$PATH":"$HOME/.pub-cache/bin"
-
-let VERSION_CODE="$(cider version | cut -d'+' -f2)" 
-let VERSION_CODE=++VERSION_CODE
-
-cider version "$1+$VERSION_CODE"
