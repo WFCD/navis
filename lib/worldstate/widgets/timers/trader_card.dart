0114d6354aa55c7dae36da95873c957a469bee8b
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-02-15 08:46:30 -0500

    fix: baro's timer wasn't formatted all the way

--- lib/worldstate/widgets/timers/trader_card.dart
+++ lib/worldstate/widgets/timers/trader_card.dart
@@ -34,20 +34,16 @@ class TraderCard extends StatelessWidget {
         };
 
         final isActive = trader?.active ?? false;
-        final date =
-            isActive ? trader?.expiry ?? now : trader?.activation ?? now;
-
+        final date = isActive ? trader?.expiry ?? now : trader?.activation ?? now;
+        final dateFormatted = MaterialLocalizations.of(context).formatFullDate(date);
         final status = isActive ? l10n.baroLeavesOn : l10n.baroArrivesOn;
-        final title = '${l10n.baroTitle} '
-            '${isActive ? '| ${trader?.location ?? ''}' : ''}';
+        final title = '${l10n.baroTitle} ${isActive ? '| ${trader?.location ?? ''}' : ''}';
 
         return Card(
           clipBehavior: Clip.antiAlias,
           color: const Color(0xFF82598b),
           child: InkWell(
-            onTap: isActive
-                ? () => TraderPageRoute(trader?.inventory).push<void>(context)
-                : null,
+            onTap: isActive ? () => TraderPageRoute(trader?.inventory).push<void>(context) : null,
             child: SizedBox(
               height: 150,
               child: ImageContainer(
@@ -55,13 +51,12 @@ class TraderCard extends StatelessWidget {
                 padding: EdgeInsets.zero,
                 child: ListTile(
                   title: Text(title),
-                  subtitle: Text('$status $date'),
+                  subtitle: Text('$status $dateFormatted'),
                   textColor: Colors.white,
                   trailing: CountdownTimer(
                     tooltip: l10n.countdownTooltip(date),
                     color: const Color(0xFF82598b),
-                    expiry:
-                        (isActive ? trader?.expiry : trader?.activation) ?? now,
+                    expiry: (isActive ? trader?.expiry : trader?.activation) ?? now,
                   ),
                 ),
               ),
