acac725c98e79790d7f7dadf1bad8f0687ed65f2
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-05-13 17:41:03 -0400

    fix: migrated to a fork of `dynamic_colors`
    original seems to have a color bug that hasn't been fixed so using the fork until then

--- lib/app/view/app_root.dart
+++ lib/app/view/app_root.dart
@@ -1,4 +1,4 @@
-import 'package:dynamic_color/dynamic_color.dart';
+import 'package:dynamic_system_colors/dynamic_system_colors.dart';
 import 'package:feedback_sentry/feedback_sentry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
