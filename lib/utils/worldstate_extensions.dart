08b95b021d0b74cdf314bff530ec699b8e1d3484
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2026-01-15 16:20:59 -0500

    fix: filter out expired events

--- lib/utils/worldstate_extensions.dart
+++ lib/utils/worldstate_extensions.dart
@@ -4,6 +4,8 @@ extension WorldstateX on Worldstate {
   void clean() {
     alerts.retainWhere((e) => e.isActive);
 
+    events.retainWhere((e) => e.isActive);
+
     news
       ..retainWhere((n) => !n.isCommunity)
       ..sort((a, b) => b.date.compareTo(a.date));
@@ -20,9 +22,9 @@ extension WorldstateX on Worldstate {
     invasions.retainWhere((e) => !e.isComplete);
 
     // Not sure why some items cost 1 plat when in-game they don't so best to filter those out until I understand why
-    const platThreshold = 1;
+    const minimumPlat = 1;
     flashSales
-      ..retainWhere((s) => s.shownInMarket && s.premiumOverride > platThreshold)
+      ..retainWhere((s) => s.shownInMarket && s.premiumOverride > minimumPlat)
       ..sort((a, b) => a.expiry.compareTo(b.expiry));
 
     calendar.days.removeWhere((d) => d.events.isEmpty);
