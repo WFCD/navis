081770131003a7119eccbc642a52796ceb036afc
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2026-02-14 13:17:46 -0500

    fix(inventoria): unable to parse users with hidden slot items

--- lib/app/view/app_root.dart
+++ lib/app/view/app_root.dart
@@ -1,4 +1,4 @@
-import 'package:dynamic_system_colors/dynamic_system_colors.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:navis/l10n/l10n.dart';
