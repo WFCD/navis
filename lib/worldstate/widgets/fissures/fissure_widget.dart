b47f53efd811d693ba7ca42fd874ea777d9025b1
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2026-04-30 23:27:37 -0400

    fix: maintenance release
    Signed-off-by: SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>

--- lib/worldstate/widgets/fissures/fissure_widget.dart
+++ lib/worldstate/widgets/fissures/fissure_widget.dart
@@ -3,9 +3,11 @@ import 'dart:math';
 
 import 'package:black_hole_flutter/black_hole_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:navis/l10n/l10n.dart';
 import 'package:navis_ui/navis_ui.dart';
 import 'package:warframe_icons/warframe_icons.dart';
+
 import 'package:worldstate_models/worldstate_models.dart';
 
 class FissureWidget extends StatelessWidget {
@@ -18,26 +20,21 @@ class FissureWidget extends StatelessWidget {
     const iconSize = 100.0;
     const opacity = .30;
 
-    final icon = () {
-      switch (fissure.tierNum) {
-        case 1:
-          return WarframeIcons.fissuresLith;
-        case 2:
-          return WarframeIcons.fissuresMeso;
-        case 3:
-          return WarframeIcons.fissuresNeo;
-        case 4:
-          return WarframeIcons.fissuresAxi;
-        case 5:
-          return WarframeIcons.fissuresRequiem;
-      }
-    }();
+    final child = _FissureContent(
+      fissure: fissure,
+      opacity: opacity,
+      iconSize: iconSize,
+      icon: switch (fissure.tier) {
+        .lith => WarframeIcons.fissuresLith,
+        .meso => WarframeIcons.fissuresMeso,
+        .neo => WarframeIcons.fissuresNeo,
+        .axi => WarframeIcons.fissuresAxi,
+        .requiem => WarframeIcons.fissuresRequiem,
+        .omnia || .unknown => WarframeIcons.nightmare,
+      },
+    );
 
-    final child = _FissureContent(fissure: fissure, opacity: opacity, iconSize: iconSize, icon: icon);
-
-    if (fissure.tierNum == 6) {
-      return GlitchySkyCard(node: fissure.node, child: child);
-    }
+    if (fissure.tier == .omnia) return GlitchySkyCard(node: fissure.node, child: child);
 
     return SkyboxCard(node: fissure.node, child: child);
   }
@@ -74,7 +71,7 @@ class _FissureContent extends StatelessWidget {
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: fissure.tierNum == 6 ? const OmniaFissureWidget() : Icon(icon, size: 40),
+              child: fissure.tier == .omnia ? const OmniaFissureWidget() : Icon(icon, size: 40),
             ),
             Expanded(child: _FissureInfo(fissure: fissure)),
             CountdownTimer(tooltip: context.l10n.countdownTooltip(fissure.expiry), expiry: fissure.expiry),
@@ -92,6 +89,8 @@ class _FissureInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final tierString = toBeginningOfSentenceCase(fissure.tier.name);
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8),
       child: Column(
@@ -102,7 +101,7 @@ class _FissureInfo extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.only(top: 4),
             child: Text(
-              '${fissure.tier} | ${fissure.missionType}',
+              '$tierString | ${fissure.missionType}',
               style: context.theme.textTheme.bodyMedium?.copyWith(color: context.theme.textTheme.bodySmall!.color),
             ),
           ),
